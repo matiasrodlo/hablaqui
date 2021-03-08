3a667e86e9fa975febcd216f569bfb40939e678a
Jesus Garcia <fairplaay16@gmail.com>
2021-03-08 16:18:03 -0400

    project

---
+++ front/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		// Rules for prettier
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				printWidth: 100,
+				useTabs: true,
+				tabWidth: 4,
+				semi: true,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				trailingComma: 'es5',
+			},
+		],
+		// Insert the property you want to deactivate here
+		'vue/require-default-prop': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	},
+};
