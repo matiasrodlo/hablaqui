350df4b8daabb30b0cdf5556aeba32e7fa3b6048
Jesus Garcia <fairplaay16@gmail.com>
2021-03-08 10:57:09 -0400

    layout

--- landing/layouts/default.vue
+++ landing/layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-app-bar fixed color="info" dark>
+      <v-btn fab small nuxt-link to="/" color="white" />
+      <span class="ml-2">LOGO</span>
+      <v-spacer></v-spacer>
+      <v-btn text small class="text-capitalize">Hazte miembro</v-btn>
+      <v-btn text small class="text-capitalize">Empezar</v-btn>
+      <v-btn text small class="text-capitalize">Nuestros especialistas</v-btn>
+      <v-btn text small class="text-capitalize">Sobre nosotros</v-btn>
+      <v-btn color="white" light small class="text-capitalize" rounded>
+        Iniciar sesión
+      </v-btn>
+    </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
--- landing/pages/index.vue
+++ landing/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col>Hola</v-col>
+    <v-col class="info--text headline">Hola</v-col>
     <v-btn color="primary">texto</v-btn>
     <v-btn color="secondary">texto</v-btn>
   </v-row>
