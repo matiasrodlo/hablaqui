842a76a2a599712cd0de8d3d57ab67b79319de82
Jesus Garcia <fairplaay16@gmail.com>
2021-03-08 10:39:32 -0400

    vuetify

--- landing/nuxt.config.js
+++ landing/nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -48,20 +46,18 @@ export default {
     },
   },
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
+        light: {
+          primary: '#6786ff',
+          secondary: '#4ac193',
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          primary: '#6786ff',
+          secondary: '#4ac193',
         },
       },
     },
