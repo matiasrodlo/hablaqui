2d37fb592244da9307d0af546d87fbc7bc833612
Jesus Garcia <fairplaay16@gmail.com>
2021-03-09 09:04:11 -0400

    linter

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,25 +1,41 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	rules: {
+		// Rules for prettier
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				printWidth: 100,
+				useTabs: true,
+				tabWidth: 4,
+				semi: true,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				trailingComma: 'es5',
+			},
+		],
+		// Insert the property you want to deactivate here
+		'vue/require-default-prop': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
 };
