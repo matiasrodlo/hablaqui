c89646446fe6d5875c5b86fa341680d185efcefb
Jesus Garcia <fairplaay16@gmail.com>
2021-03-09 09:19:56 -0400

    router work

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -7,23 +7,23 @@ Vue.use(VueRouter);
 const routes = [
 	{
 		path: '/',
-		name: 'Home',
+		name: 'especialistas',
 		component: Home,
+		meta: { title: 'Nuestros especialistas' },
 	},
 	{
-		path: '/about',
-		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+		path: '*',
+		name: 'Error',
+		component: require('@/views/404').default, // load sync
+		meta: { title: '404' },
 	},
 ];
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
 	routes,
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
 });
 
 export default router;
---
+++ front/src/views/404.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="error--text">
+		<h1>Pagina no encontrada - error 404</h1>
+	</div>
+</template>
