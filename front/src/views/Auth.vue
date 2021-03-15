532721dd66cb48cdbc377f403138dd133aa79044
Jesus Garcia <fairplaay16@gmail.com>
2021-03-15 10:48:36 -0400

    windows items

--- front/src/views/Auth.vue
+++ front/src/views/Auth.vue
@@ -33,63 +33,41 @@
 						</v-col>
 					</v-row>
 				</v-col>
-				<v-col
-					v-if="$vuetify.breakpoint.smAndUp"
-					cols="12"
-					sm="6"
-					class="login-circle-image"
-				>
-					<v-row justify="center">
-						<v-col cols="8" md="7" class="text-center">
-							<v-list-item-avatar
-								size="200"
-								class="ml-4 "
-								style="border: 10px solid white"
-							>
-								<v-btn
-									color="#9D9D9C"
-									class="login-plus-image elevation-0"
-									fab
-									width="200"
-									height="200"
-								></v-btn>
-							</v-list-item-avatar>
-						</v-col>
-					</v-row>
-					<v-row justify="center" class="text-center white--text">
-						<v-col cols="6">
-							Lorem ipsum dolor sit amet consectetur, fuga incidunt distinctio
-							laudantium impedit voluptate.
-						</v-col>
-					</v-row>
-					<v-row justify="center" class="text-center white--text">
-						<v-col cols="6">
-							<v-list-item-avatar
-								size="10"
-								class="ma-2"
-								style="border: 10px solid white"
-							>
-							</v-list-item-avatar>
-							<v-list-item-avatar
-								size="10"
-								class="ma-2"
-								style="border: 10px solid white"
-							>
-							</v-list-item-avatar>
-							<v-list-item-avatar
-								size="10"
-								class="ma-2"
-								style="border: 10px solid white"
-							>
-							</v-list-item-avatar>
-							<v-list-item-avatar
-								size="10"
-								class="ma-2"
-								style="border: 10px solid white"
-							>
-							</v-list-item-avatar>
-						</v-col>
-					</v-row>
+				<v-col v-if="$vuetify.breakpoint.smAndUp" sm="6">
+					<v-window v-model="onboarding" class="login-circle-image">
+						<v-window-item
+							v-for="n in length"
+							:key="`card-${n}`"
+							class="login-plus-image"
+						>
+							<div class="text-center ">
+								<v-list-item-avatar
+									size="200"
+									class="ml-4 "
+									style="border: 10px solid white"
+								>
+									<v-btn
+										color="#9D9D9C"
+										class="elevation-0"
+										fab
+										width="200"
+										height="200"
+									></v-btn>
+								</v-list-item-avatar>
+							</div>
+							<div class="text-center white--text py-4">
+								Lorem ipsum dolor sit amet consectetur, fuga incidunt distinctio
+								laudantium impedit voluptate.
+							</div>
+						</v-window-item>
+					</v-window>
+					<v-item-group v-model="onboarding" class="text-center" mandatory>
+						<v-item v-for="n in length" :key="`btn-${n}`" v-slot="{ active, toggle }">
+							<v-btn :input-value="active" icon @click="toggle" color="#BDBDBD">
+								<v-icon>mdi-record</v-icon>
+							</v-btn>
+						</v-item>
+					</v-item-group>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -105,6 +83,8 @@ export default {
 	},
 	data() {
 		return {
+			length: 4,
+			onboarding: 0,
 			menu: false,
 			step: 1,
 		};
@@ -120,6 +100,12 @@ export default {
 			if (this.step == 1) this.step = 2;
 			else this.step = 1;
 		},
+		next() {
+			this.onboarding = this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
+		},
+		prev() {
+			this.onboarding = this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
+		},
 	},
 };
 </script>
@@ -127,19 +113,19 @@ export default {
 <style lang="scss" scoped>
 .login-circle-image {
 	background: url('../assets/circle-login.png') no-repeat;
-	background-position-y: 90%;
-	background-position-x: 70%;
-	-webkit-background-size: 30%;
-	-moz-background-size: 30%;
-	-o-background-size: 30%;
-	background-size: 30%;
+	background-position-y: 70%;
+	background-position-x: 60%;
+	-webkit-background-size: 15%;
+	-moz-background-size: 15%;
+	-o-background-size: 15%;
+	background-size: 15%;
 }
 .login-plus-image {
 	background: url('../assets/plus-login.png') no-repeat;
-	background-position-x: 60%;
-	-webkit-background-size: 20%;
-	-moz-background-size: 20%;
-	-o-background-size: 20%;
-	background-size: 20%;
+	background-position-x: 65%;
+	-webkit-background-size: 7%;
+	-moz-background-size: 7%;
+	-o-background-size: 7%;
+	background-size: 7%;
 }
 </style>
