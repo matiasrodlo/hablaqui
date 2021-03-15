50b48017d639a466a75bf0724e5f7e7c2a3212de
Jesus Garcia <fairplaay16@gmail.com>
2021-03-15 12:44:14 -0400

    hablaqui faq

--- front/src/views/Faq.vue
+++ front/src/views/Faq.vue
@@ -1,7 +1,40 @@
 <template>
-	<div>
-		Faq
-	</div>
+	<v-container fluid>
+		<v-row justify="center">
+			<v-col
+				cols="12"
+				class="text-center font-weight-bold text-h5 text-lg-h3 text--secondary"
+			>
+				Bienvenido a nuestro portal de ayuda
+			</v-col>
+			<v-col cols="12" md="10" xl="8">
+				<v-text-field
+					class="white"
+					label="Busca por tema o pregunta"
+					outlined
+					hide-details=""
+				/>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col cols="12" sm="6" lg="3" v-for="el in [1, 2, 3, 4]" :key="el">
+				<v-card>
+					<v-card-title class="text--secondary font-weight-bold title">
+						¿Qué es y como funciona Hablaquí?
+						<div class="caption font-weight-bold" style="color:#BDBDBD">
+							5 artículos sobre el tema
+						</div>
+					</v-card-title>
+					<v-divider></v-divider>
+					<v-card-text>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptas aut
+						incidunt sequi alias rerum suscipit harum assumenda itaque modi, nobis fugit
+						vitae eaque eius animi molestiae aperiam, at ducimus.
+					</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
