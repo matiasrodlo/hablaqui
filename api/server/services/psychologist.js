d6bab635d0bada0990179940b7156ea6bce481e7
torokokoo <torokokov7@gmail.com>
2021-04-07 17:08:51 -0400

    fix logInfo

--- api/server/services/psychologist.js
+++ api/server/services/psychologist.js
@@ -1,6 +1,5 @@
 import { logInfo } from '../config/pino';
 import Psychologist from '../models/psychologist';
-import { infoMessages } from '../utils/logger/infoMessages';
 import { okResponse } from '../utils/responses/functions';
 
 const getAll = async () => {
