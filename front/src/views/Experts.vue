a6217f559a80f45a7c96ea4b39f2762612b3bbef
Jesus Garcia <fairplaay16@gmail.com>
2021-04-08 15:19:59 -0400

    recruitments view ok

--- front/src/views/Experts.vue
+++ front/src/views/Experts.vue
@@ -14,51 +14,7 @@
 				</v-col>
 			</v-row>
 		</v-container>
-		<v-container fluid>
-			<v-row class="white--text py-16">
-				<v-col cols="12" sm="3">
-					<div class="text-center headline text--secondary font-weight-bold">
-						Acuerdos
-					</div>
-					<div class="text-center headline text--secondary">
-						Políticas de Privacidad
-					</div>
-					<div class="text-center headline text--secondary">
-						Terminos y Condiciones
-					</div>
-				</v-col>
-				<v-col cols="12" sm="3">
-					<div class="text-center headline text--secondary font-weight-bold">
-						Soporte Técnico
-					</div>
-					<div class="text-center headline text--secondary">
-						+52 1 5568 2686 6322
-					</div>
-					<div class="text-center headline text--secondary">
-						soporte@hablaqui.com
-					</div>
-				</v-col>
-				<v-col cols="12" sm="3">
-					<div class="text-center headline text--secondary font-weight-bold">
-						Atención a clientes
-					</div>
-					<div class="text-center headline text--secondary">
-						clientes@hablaqui.com
-					</div>
-					<div class="text-center headline text--secondary">
-						John Harvard
-					</div>
-				</v-col>
-				<v-col cols="12" sm="3">
-					<div class="text-center headline text--secondary font-weight-bold">
-						Pago seguro
-					</div>
-					<div class="text-center headline text--secondary">
-						LOGO PAYPAL
-					</div>
-				</v-col>
-			</v-row>
-		</v-container>
+		<Footer />
 	</div>
 </template>
 
@@ -69,6 +25,9 @@ export default {
 		return {
 			loading: false,
 		};
+	},
+	components: {
+		Footer: () => import('@/components/footers/FooterExperts'),
 	},
 	async mounted() {
 		this.loading = true;
