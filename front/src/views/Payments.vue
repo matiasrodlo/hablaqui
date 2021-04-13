cd9ed78564ba24bcd50736e34877ab686aa98986
Jesus Garcia <fairplaay16@gmail.com>
2021-04-13 10:36:23 -0400

    card 1

--- front/src/views/Payments.vue
+++ front/src/views/Payments.vue
@@ -1,116 +1,150 @@
 <template>
-	<v-container fluid>
+	<v-container>
 		<v-row align="center" justify="center">
-			<ul id="breadcrumb">
-				<li :class="breakCrumbs == 0 ? 'child-selected' : 'child-un-selected'">
-					<span>Agendar</span>
-				</li>
-				<li>
-					<span :class="breakCrumbs == 1 ? 'child-selected' : 'child-un-selected'">
-						Escoger Plan
-					</span>
-				</li>
-				<li :class="breakCrumbs == 2 ? 'child-selected' : 'child-un-selected'">
-					<span>Detalles</span>
-				</li>
-				<li :class="breakCrumbs == 3 ? 'child-selected' : 'child-un-selected'">
-					<span>Pago</span>
-				</li>
-			</ul>
 			<v-col cols="12">
 				<div class="text-center text-h4 font-weight-bold">Lorem ipsum dolor</div>
 				<div class="text-center caption font-weight-bold">
 					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
 				</div>
 			</v-col>
-			<template v-if="showLoading">
-				<v-col cols="12" sm="12" md="2" class="text-center text-md-right">
-					<v-progress-circular indeterminate color="primary" size="130" :width="15">
-						<div class="spinner"></div>
-					</v-progress-circular>
-				</v-col>
-				<v-col cols="12" sm="12" md="6" lg="5" xl="4" class="text-center text-md-left">
-					<div v-for="el in 4" :key="el" class="my-3 font-weight-bold">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit
-						<v-badge color="primary" class="mx-2"></v-badge>
+			<v-col cols="12" md="2" class="text-center text-md-right">
+				<v-progress-circular indeterminate color="primary" size="130" :width="15">
+					<div class="spinner"></div>
+				</v-progress-circular>
+			</v-col>
+			<v-col cols="12" md="6" lg="5" xl="4" class="text-center text-md-left">
+				<div v-for="el in 4" :key="el" class="my-3 font-weight-bold">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit
+					<v-badge color="primary" class="mx-2"></v-badge>
+				</div>
+			</v-col>
+		</v-row>
+		<v-row align="center" justify="center">
+			<v-col cols="12" v-for="element in 2" :key="element" class="d-flex justify-center">
+				<v-card max-width="800" color="white" style="border-radius: 15px">
+					<v-card-text>
+						<v-row align="center" justify="center">
+							<v-col cols="12" sm="3" class="text-center">
+								<v-list-item-avatar
+									:size="$vuetify.breakpoint.mdAndUp ? '180' : '100'"
+									class="ml-4"
+								>
+									<v-btn
+										color="#9D9D9C"
+										class="elevation-0"
+										fab
+										width="100"
+										height="100"
+									></v-btn>
+								</v-list-item-avatar>
+								<div class="caption text--secondary">
+									cedula xxxxxx
+								</div>
+								<v-btn text color="primary">Mas información</v-btn>
+							</v-col>
+							<v-col cols="12" sm="9">
+								<v-row justify="space-between">
+									<v-col
+										class="text-center text-sm-left font-weight-bold text-h5 text--secondary"
+									>
+										Fernanda Croffman
+									</v-col>
+									<v-col cols="12" sm="6" class="text-right">
+										<v-btn color="primary" rounded>
+											Agenda cita online
+										</v-btn>
+									</v-col>
+								</v-row>
+								<v-chip v-for="el in [3, 1, 2]" :key="el" small class="my-4 mx-1">
+									Ansiedad
+								</v-chip>
+								<div class="body-2 mt-2 text-capitalize">
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+									diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+									aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+									nostrud exerci tation ullamcorper suscipit loborm dolor sit
+									amet, consectetuer adipiscing elit, sed diam nonummy nibh
+									euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+									Ut wisi enim ad minim veniam, qum dolor sit amet, consectetuer
+									adipiscing elit, sed diam nonummy
+								</div>
+							</v-col>
+						</v-row>
+					</v-card-text>
+				</v-card>
+			</v-col>
+			<v-col cols="12" class="font-weight-bold mt-4 mb-16 text-center">
+				<span class="primary--text">¿No estas felíz con las opciones?</span> Mostrar mas
+				psicólogos Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no
+			</v-col>
+		</v-row>
+		<v-row justify="center" align="center">
+			<v-col cols="12">
+				<div class="text-center">
+					<ul id="breadcrumb">
+						<li :class="breakCrumbs == 0 ? 'child-selected' : 'child-un-selected'">
+							<span>Agendar</span>
+						</li>
+						<li>
+							<span
+								:class="breakCrumbs == 1 ? 'child-selected' : 'child-un-selected'"
+							>
+								Escoger Plan
+							</span>
+						</li>
+						<li :class="breakCrumbs == 2 ? 'child-selected' : 'child-un-selected'">
+							<span>Detalles</span>
+						</li>
+						<li :class="breakCrumbs == 3 ? 'child-selected' : 'child-un-selected'">
+							<span>Pago</span>
+						</li>
+					</ul>
+				</div>
+			</v-col>
+			<v-col cols="12" style="max-width: 800px">
+				<div class="text-secondary mt-16 text-h4 font-weight-bold">
+					Lorem ipsum dolor sit
+				</div>
+				<div class="text-secondary text-h6 font-weight-bold">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonum-my nibh
+					ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonum-
+				</div>
+			</v-col>
+			<v-col cols="12" v-for="(el, i) in plans" :key="i" class="d-flex justify-center">
+				<v-card max-width="800">
+					<div v-if="el.recommended" class="d-flex align-center justify-end">
+						<span
+							class="pa-2 primary white--text font-weight-bold"
+							style="border-radius: 0 0 0 15px"
+						>
+							Recomendado
+						</span>
 					</div>
-				</v-col>
-			</template>
-			<template v-else>
-				<v-col cols="8" v-for="element in 3" :key="element">
-					<v-card color="white" style="border-radius: 15px">
-						<v-card-text>
-							<v-row align="center" justify="center">
-								<v-col cols="12" sm="3" class="text-center">
-									<v-list-item-avatar
-										:size="$vuetify.breakpoint.mdAndUp ? '180' : '100'"
-										class="ml-4"
-									>
-										<v-btn
-											color="#9D9D9C"
-											class="elevation-0"
-											fab
-											width="100"
-											height="100"
-										></v-btn>
-									</v-list-item-avatar>
-									<div class="caption text--secondary">
-										cedula xxxxxx
-									</div>
-									<v-btn text color="primary">Mas información</v-btn>
-								</v-col>
-								<v-col cols="12" sm="9">
-									<v-row justify="space-between">
-										<v-col
-											class="text-center text-sm-left font-weight-bold text-h5 text--secondary"
-										>
-											Fernanda Croffman
-										</v-col>
-										<v-col cols="12" sm="6" class="text-right">
-											<v-btn color="primary" rounded>
-												Agenda cita online
-											</v-btn>
-										</v-col>
-									</v-row>
-									<v-chip
-										v-for="el in [3, 1, 2]"
-										:key="el"
-										small
-										class="my-4 mx-1"
-									>
-										Ansiedad
-									</v-chip>
-									<div class="body-2 mt-2 text-capitalize">
-										Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-										sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-										magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-										quis nostrud exerci tation ullamcorper suscipit loborm dolor
-										sit amet, consectetuer adipiscing elit, sed diam nonummy
-										nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-										volutpat. Ut wisi enim ad minim veniam, qum dolor sit amet,
-										consectetuer adipiscing elit, sed diam nonummy
-									</div>
-								</v-col>
-							</v-row>
-						</v-card-text>
-					</v-card>
-				</v-col>
-				<v-col cols="12" class="font-weight-bold mt-4 mb-16 text-center">
-					<span class="primary--text">¿No estas felíz con las opciones?</span> Mostrar mas
-					psicólogos Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no
-				</v-col>
-			</template>
-			<!-- <v-col cols="12">
-				<v-window v-model="onboarding" reverse>
-					<v-window-item v-for="n in 3" :key="`card-${n}`">
-						<v-card color="grey" height="200">
-							<v-row class="fill-height" align="center" justify="center">
-								<h1 style="font-size: 5rem;" class="white--text">Slide {{ n }}</h1>
-							</v-row>
-						</v-card>
-					</v-window-item>
-				</v-window>
-			</v-col> -->
+					<v-card-text>
+						<v-row justify="space-between" align="center">
+							<v-col>
+								<span class="text-h3">{{ el.price }} $</span>
+								<span class="text-h6 text--secondary">/{{ el.mode }}</span>
+								<div class="text-h6 primary--text font-weight-bold">
+									{{ el.title }}
+								</div>
+								<div class="my-2 font-weight-bold">
+									{{ el.subtitle }}
+								</div>
+								<div class="body-1 mt-2">
+									{{ el.description }}
+								</div>
+							</v-col>
+							<v-col cols="3" class="text-center mt-6">
+								<v-avatar color="primary" size="90"></v-avatar>
+								<v-btn class="mt-3" color="primary" text>
+									Seleccionar plan
+								</v-btn>
+							</v-col>
+						</v-row>
+					</v-card-text>
+				</v-card>
+			</v-col>
 		</v-row>
 	</v-container>
 </template>
@@ -121,6 +155,26 @@ export default {
 		return {
 			breakCrumbs: 0,
 			showLoading: true,
+			plans: [
+				{
+					recommended: true,
+					price: 14,
+					mode: 'week',
+					title: 'Lorem ipsum dolor sit amet, consec',
+					subtitle: 'Lorem ipsum dolor sit amet, consecte',
+					description:
+						'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no-nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper ',
+				},
+				{
+					recommended: false,
+					price: 9,
+					mode: 'week',
+					title: 'Lorem ipsum dolor sit amet, consec',
+					subtitle: 'Lorem ipsum dolor sit amet, consecte',
+					description:
+						'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no-nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper ',
+				},
+			],
 		};
 	},
 	mounted() {
@@ -149,14 +203,14 @@ $primary: #2070e5;
 	li {
 		float: left;
 		&:first-child {
-			padding-left: 15px;
+			padding-left: 50px;
 			border-radius: 15px 0 0 15px;
 			&:before {
 				border: none;
 			}
 		}
 		&:last-child {
-			padding-right: 15px;
+			padding-right: 50px;
 			border-radius: 0 15px 15px 0;
 			&:after {
 				border: none;
@@ -174,7 +228,7 @@ $primary: #2070e5;
 	position: relative;
 	height: 40px;
 	line-height: 40px;
-	padding: 0 10px 0 5px;
+	padding: 0 50px 0 50px;
 	text-align: center;
 	margin-right: 23px;
 	&:before,
@@ -207,7 +261,7 @@ $primary: #2070e5;
 	position: relative;
 	height: 40px;
 	line-height: 40px;
-	padding: 0 10px 0 5px;
+	padding: 0 50px 0 50px;
 	text-align: center;
 	margin-right: 23px;
 	&:before,
