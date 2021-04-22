359f755ac93b9ef3c98584c21cd1c6bfba8f0c73
Jesus Garcia <fairplaay16@gmail.com>
2021-04-22 12:37:56 -0400

    divs

--- landing/pages/index.vue
+++ landing/pages/index.vue
@@ -4,8 +4,10 @@
 			<Appbar />
 		</div>
 		<!-- section 1 -->
-		<v-img src="container-blue.png" contain>
-			<div class="primary">
+		<div style="position: relative">
+			<img src="container-blue.png" style="height: 500px; width: 100%" />
+			<div style="position: absolute; top: 0; width: 100%">
+				<div class="primary" style="height: 45px"></div>
 				<v-container class="white--text" style="height: 400px">
 					<v-row justify="center" align="center" style="height: 400px">
 						<v-col cols="8" sm="6" xl="5" class="text-center text-sm-left">
@@ -40,7 +42,7 @@
 					</v-row>
 				</v-container>
 			</div>
-		</v-img>
+		</div>
 		<!-- section 2 -->
 		<v-container class="pt-10">
 			<v-row justify="center">
@@ -92,36 +94,39 @@
 			</v-row>
 		</v-container>
 		<!-- section 3 -->
-		<v-img src="container-blue.png" contain>
-			<v-container class="white--text" style="height: 400px">
-				<v-row justify="center" align="center" style="height: 400px">
-					<v-col cols="12" class="mt-16 text-center text-sm-left">
-						<div class="text-lg-h2 text-md-h3 text-h5 font-weight-bold">Planes</div>
-					</v-col>
-					<v-col cols="4" class="text-center hidden-sm-and-down">
-						<v-card flat>
-							<v-card-text> Plan $n </v-card-text>
-							<v-card-text> Descripcion </v-card-text>
-							<v-card-text> Precio $</v-card-text>
-						</v-card>
-					</v-col>
-					<v-col cols="4" class="text-center hidden-sm-and-down">
-						<v-card flat>
-							<v-card-text> Plan $n </v-card-text>
-							<v-card-text> Descripcion </v-card-text>
-							<v-card-text> Precio $</v-card-text>
-						</v-card>
-					</v-col>
-					<v-col cols="4" class="text-center hidden-sm-and-down">
-						<v-card flat>
-							<v-card-text> Plan $n </v-card-text>
-							<v-card-text> Descripcion </v-card-text>
-							<v-card-text> Precio $</v-card-text>
-						</v-card>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-img>
+		<div style="position: relative">
+			<img src="container-blue.png" style="height: 500px; width: 100%" />
+			<div style="position: absolute; top: 0; width: 100%">
+				<v-container class="white--text" style="height: 400px">
+					<v-row justify="center" align="center" style="height: 400px">
+						<v-col cols="12" class="mt-16 text-center text-sm-left">
+							<div class="text-lg-h2 text-md-h3 text-h5 font-weight-bold">Planes</div>
+						</v-col>
+						<v-col cols="4" class="text-center hidden-sm-and-down">
+							<v-card flat>
+								<v-card-text> Plan $n </v-card-text>
+								<v-card-text> Descripcion </v-card-text>
+								<v-card-text> Precio $</v-card-text>
+							</v-card>
+						</v-col>
+						<v-col cols="4" class="text-center hidden-sm-and-down">
+							<v-card flat>
+								<v-card-text> Plan $n </v-card-text>
+								<v-card-text> Descripcion </v-card-text>
+								<v-card-text> Precio $</v-card-text>
+							</v-card>
+						</v-col>
+						<v-col cols="4" class="text-center hidden-sm-and-down">
+							<v-card flat>
+								<v-card-text> Plan $n </v-card-text>
+								<v-card-text> Descripcion </v-card-text>
+								<v-card-text> Precio $</v-card-text>
+							</v-card>
+						</v-col>
+					</v-row>
+				</v-container>
+			</div>
+		</div>
 		<!-- section 4 -->
 		<v-container class="pt-10">
 			<v-row justify="center">
@@ -173,75 +178,78 @@
 			</v-row>
 		</v-container>
 		<!-- SECTION 5 -->
-		<v-img src="container-blue.png" contain>
-			<v-container
-				class="white--text"
-				:class="$vuetify.breakpoint.smAndUp ? '' : 'primary'"
-				style="height: 500px"
-			>
-				<v-row justify="center" align="center" style="height: 500px">
-					<v-col cols="12" sm="6" xl="5" class="text-center text-sm-left">
-						<div class="text-lg-h2 text-md-h3 text-h5 font-weight-bold">
-							Nuestros psicólogos
-						</div>
-						<div class="caption my-4">
-							Contamos con un amplio equipo de especialistas dedicados a lo quetú
-							busques desarrollar. Todos nuestros profesionales sonrigurosamente
-							seleccionados, certificados, y cuentan con un alto nivelde formación y
-							experiencia. Te garantizamos una atención efectiva yexperta, pero
-							también cálida y humana
-						</div>
-						<v-btn
-							rounded
-							color="white"
-							x-large
-							class="font-weight-bold"
-							depressed
-							:href="`${$config.FRONTEND_URL}/auth`"
-						>
-							QUIERO COMENZAR
-						</v-btn>
-					</v-col>
-					<v-col cols="12" sm="6" xl="5" class="text-center">
-						<v-row justify="center">
-							<v-col cols="12" class="text-center">
-								<v-btn
-									style="border: 8px solid #5eb3e4; cursor: initial"
-									depressed
-									fab
-									width="100"
-									height="100"
-								>
-									<v-avatar color="white" size="85"> </v-avatar>
-								</v-btn>
-							</v-col>
-							<v-col cols="4" sm="6" md="3">
-								<v-btn
-									style="border: 8px solid #5eb3e4; cursor: initial"
-									depressed
-									fab
-									width="100"
-									height="100"
-								>
-									<v-avatar color="white" size="85"> </v-avatar>
-								</v-btn>
-							</v-col>
-							<v-col cols="4" sm="6" md="3">
-								<v-btn
-									style="border: 8px solid #5eb3e4; cursor: initial"
-									depressed
-									fab
-									width="100"
-									height="100"
-								>
-									<v-avatar color="white" size="85"> </v-avatar>
-								</v-btn>
-							</v-col>
-						</v-row>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-img>
+		<div style="position: relative">
+			<img src="container-blue.png" style="height: 500px; width: 100%" />
+			<div style="position: absolute; top: 0; width: 100%">
+				<v-container
+					class="white--text"
+					:class="$vuetify.breakpoint.smAndUp ? '' : 'primary'"
+					style="height: 500px"
+				>
+					<v-row justify="center" align="center" style="height: 500px">
+						<v-col cols="12" sm="6" xl="5" class="text-center text-sm-left">
+							<div class="text-lg-h2 text-md-h3 text-h5 font-weight-bold">
+								Nuestros psicólogos
+							</div>
+							<div class="caption my-4">
+								Contamos con un amplio equipo de especialistas dedicados a lo quetú
+								busques desarrollar. Todos nuestros profesionales sonrigurosamente
+								seleccionados, certificados, y cuentan con un alto nivelde formación
+								y experiencia. Te garantizamos una atención efectiva yexperta, pero
+								también cálida y humana
+							</div>
+							<v-btn
+								rounded
+								color="white"
+								x-large
+								class="font-weight-bold"
+								depressed
+								:href="`${$config.FRONTEND_URL}/auth`"
+							>
+								QUIERO COMENZAR
+							</v-btn>
+						</v-col>
+						<v-col cols="12" sm="6" xl="5" class="text-center">
+							<v-row justify="center">
+								<v-col cols="12" class="text-center">
+									<v-btn
+										style="border: 8px solid #5eb3e4; cursor: initial"
+										depressed
+										fab
+										width="100"
+										height="100"
+									>
+										<v-avatar color="white" size="85"> </v-avatar>
+									</v-btn>
+								</v-col>
+								<v-col cols="4" sm="6" md="3">
+									<v-btn
+										style="border: 8px solid #5eb3e4; cursor: initial"
+										depressed
+										fab
+										width="100"
+										height="100"
+									>
+										<v-avatar color="white" size="85"> </v-avatar>
+									</v-btn>
+								</v-col>
+								<v-col cols="4" sm="6" md="3">
+									<v-btn
+										style="border: 8px solid #5eb3e4; cursor: initial"
+										depressed
+										fab
+										width="100"
+										height="100"
+									>
+										<v-avatar color="white" size="85"> </v-avatar>
+									</v-btn>
+								</v-col>
+							</v-row>
+						</v-col>
+					</v-row>
+				</v-container>
+			</div>
+		</div>
 		<v-container>
 			<v-row>
 				<v-col class="text-center">
