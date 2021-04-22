015a542b044b8495d79f2135a63745ea609104e7
torokokoo <torokokov7@gmail.com>
2021-04-22 19:06:13 -0400

    google storage + passport at uploadCsv

--- api/server/routes/data.js
+++ api/server/routes/data.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
+import passport from 'passport';
 import dataController from '../controllers/data';
+import storage from '../middleware/storage';
 
 const upload = multer({
 	dest: 'uploads/',
@@ -10,7 +12,11 @@ const dataRouter = Router();
 
 dataRouter.post(
 	'/data/upload',
-	upload.single('csvFile'),
+	[
+		passport.authenticate('jwt', { session: true }),
+		upload.single('csvFile'),
+		storage,
+	],
 	dataController.uploadCsv
 );
 
