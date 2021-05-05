88af37ae68ada1f190b9fc91be2f20e4d20ed879
Jesus Garcia <fairplaay16@gmail.com>
2021-05-05 12:07:55 -0400

    nuxt routes

---
+++ landing/pages/pagos/failure-pay.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="headline error--text">failure pay</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ landing/pages/pagos/pending-pay.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="headline warning--text">pending pay</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
