cf33cfd11de1214de2bf3a29df114ef9a3bcf2f3
Jesus Garcia <fairplaay16@gmail.com>
2021-05-11 08:49:25 -0400

    try cal

--- front/src/views/children/payments/PaymentsHome.vue
+++ front/src/views/children/payments/PaymentsHome.vue
@@ -44,28 +44,62 @@
 						</v-btn>
 					</div>
 				</v-col>
-				<v-col v-if="breakCrumbs == 0" cols="12">
+				<div v-if="breakCrumbs == 0" cols="12">
 					<div class="text-center text-secondary mt-4 text-h4 font-weight-bold">
 						Agenda la hora y día de tu consulta
 					</div>
 					<div class="text-center text-secondary text-h6 font-weight-bold">
 						Agenda con total libertad cuando te resulte más conveniente.
 					</div>
-					<v-row justify="center">
-						<v-col cols="9" sm="6" md="6">
-							<v-date-picker full-width v-model="picker" locale="es"></v-date-picker>
-						</v-col>
-						<v-col cols="4">
-							<v-sheet height="400">
-								<v-calendar v-model="picker" color="primary" type="day">
-									<template v-slot:interval="{ hour }">
-										<div class="text-center">{{ hour }}</div>
-									</template>
-								</v-calendar>
-							</v-sheet>
-						</v-col>
-					</v-row>
-				</v-col>
+					<v-card
+						class="mt-10"
+						elevation="10"
+						max-width="700"
+						style="border-radius: 25px"
+					>
+						<v-card-text>
+							<v-row>
+								<v-col cols="8">
+									<v-date-picker
+										full-width
+										v-model="picker"
+										locale="es"
+									></v-date-picker>
+								</v-col>
+								<v-col cols="4">
+									<v-sheet height="400">
+										<v-calendar
+											ref="calendar"
+											v-model="value"
+											type="day"
+											hide-header
+											:events="events"
+											:event-ripple="false"
+											@change="getEvents"
+											@mousedown:event="startDrag"
+											@mousedown:time="startTime"
+											@mousemove:time="mouseMove"
+											@mouseup:time="endDrag"
+											@mouseleave.native="cancelDrag"
+										>
+											<template v-slot:event="{ event, timed, eventSummary }">
+												<div
+													class="v-event-draggable"
+													v-html="eventSummary()"
+												></div>
+												<div
+													v-if="timed"
+													class="v-event-drag-bottom"
+													@mousedown.stop="extendBottom(event)"
+												></div>
+											</template>
+										</v-calendar>
+									</v-sheet>
+								</v-col>
+							</v-row>
+						</v-card-text>
+					</v-card>
+				</div>
 				<v-col cols="12" v-if="breakCrumbs == 1">
 					<v-row>
 						<v-col
@@ -391,9 +425,9 @@ export default {
 	},
 	data() {
 		return {
+			timePicker: '',
 			picker: '',
 			breakCrumbs: 0,
-			showLoading: true,
 			plans: [
 				{
 					deals: [
@@ -425,12 +459,163 @@ export default {
 						'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no-nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper ',
 				},
 			],
+			value: '',
+			events: [],
+			colors: ['#2196F3', '#3F51B5', '#673AB7', '#00BCD4', '#4CAF50', '#FF9800', '#757575'],
+			names: [
+				'Meeting',
+				'Holiday',
+				'PTO',
+				'Travel',
+				'Event',
+				'Birthday',
+				'Conference',
+				'Party',
+			],
+			dragEvent: null,
+			dragStart: null,
+			createEvent: null,
+			createStart: null,
+			extendOriginal: null,
 		};
 	},
-	mounted() {
-		setTimeout(() => {
-			this.showLoading = false;
-		}, 2000);
+	methods: {
+		setTime(e) {
+			console.log(e);
+		},
+		startDrag({ event, timed }) {
+			if (event && timed) {
+				this.dragEvent = event;
+				this.dragTime = null;
+				this.extendOriginal = null;
+			}
+		},
+		startTime(tms) {
+			const mouse = this.toTime(tms);
+
+			if (this.dragEvent && this.dragTime === null) {
+				const start = this.dragEvent.start;
+
+				this.dragTime = mouse - start;
+			} else {
+				this.createStart = this.roundTime(mouse);
+				this.createEvent = {
+					name: `Event #${this.events.length}`,
+					color: this.rndElement(this.colors),
+					start: this.createStart,
+					end: this.createStart,
+					timed: true,
+				};
+
+				this.events.push(this.createEvent);
+			}
+		},
+		extendBottom(event) {
+			this.createEvent = event;
+			this.createStart = event.start;
+			this.extendOriginal = event.end;
+		},
+		mouseMove(tms) {
+			const mouse = this.toTime(tms);
+
+			if (this.dragEvent && this.dragTime !== null) {
+				const start = this.dragEvent.start;
+				const end = this.dragEvent.end;
+				const duration = end - start;
+				const newStartTime = mouse - this.dragTime;
+				const newStart = this.roundTime(newStartTime);
+				const newEnd = newStart + duration;
+
+				this.dragEvent.start = newStart;
+				this.dragEvent.end = newEnd;
+			} else if (this.createEvent && this.createStart !== null) {
+				const mouseRounded = this.roundTime(mouse, false);
+				const min = Math.min(mouseRounded, this.createStart);
+				const max = Math.max(mouseRounded, this.createStart);
+
+				this.createEvent.start = min;
+				this.createEvent.end = max;
+			}
+		},
+		endDrag() {
+			this.dragTime = null;
+			this.dragEvent = null;
+			this.createEvent = null;
+			this.createStart = null;
+			this.extendOriginal = null;
+		},
+		cancelDrag() {
+			if (this.createEvent) {
+				if (this.extendOriginal) {
+					this.createEvent.end = this.extendOriginal;
+				} else {
+					const i = this.events.indexOf(this.createEvent);
+					if (i !== -1) {
+						this.events.splice(i, 1);
+					}
+				}
+			}
+
+			this.createEvent = null;
+			this.createStart = null;
+			this.dragTime = null;
+			this.dragEvent = null;
+		},
+		roundTime(time, down = true) {
+			const roundTo = 15; // minutes
+			const roundDownTime = roundTo * 60 * 1000;
+
+			return down
+				? time - (time % roundDownTime)
+				: time + (roundDownTime - (time % roundDownTime));
+		},
+		toTime(tms) {
+			return new Date(tms.year, tms.month - 1, tms.day, tms.hour, tms.minute).getTime();
+		},
+		getEventColor(event) {
+			const rgb = parseInt(event.color.substring(1), 16);
+			const r = (rgb >> 16) & 0xff;
+			const g = (rgb >> 8) & 0xff;
+			const b = (rgb >> 0) & 0xff;
+
+			return event === this.dragEvent
+				? `rgba(${r}, ${g}, ${b}, 0.7)`
+				: event === this.createEvent
+				? `rgba(${r}, ${g}, ${b}, 0.7)`
+				: event.color;
+		},
+		getEvents({ start, end }) {
+			const events = [];
+
+			const min = new Date(`${start.date}T00:00:00`).getTime();
+			const max = new Date(`${end.date}T23:59:59`).getTime();
+			const days = (max - min) / 86400000;
+			const eventCount = this.rnd(days, days + 20);
+
+			for (let i = 0; i < eventCount; i++) {
+				const timed = this.rnd(0, 3) !== 0;
+				const firstTimestamp = this.rnd(min, max);
+				const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000;
+				const start = firstTimestamp - (firstTimestamp % 900000);
+				const end = start + secondTimestamp;
+
+				events.push({
+					name: this.rndElement(this.names),
+					color: this.rndElement(this.colors),
+					start,
+					end,
+					timed,
+				});
+			}
+
+			this.events = events;
+		},
+		rnd(a, b) {
+			return Math.floor((b - a + 1) * Math.random()) + a;
+		},
+		rndElement(arr) {
+			return arr[this.rnd(0, arr.length - 1)];
+		},
 	},
 };
 </script>
@@ -532,6 +717,41 @@ $primary: #2070e5;
 		left: 100%;
 		border-color: transparent;
 		border-left-color: $info;
+	}
+}
+
+.v-event-draggable {
+	padding-left: 6px;
+}
+
+.v-event-timed {
+	user-select: none;
+	-webkit-user-select: none;
+}
+
+.v-event-drag-bottom {
+	position: absolute;
+	left: 0;
+	right: 0;
+	bottom: 4px;
+	height: 4px;
+	cursor: ns-resize;
+
+	&::after {
+		display: none;
+		position: absolute;
+		left: 50%;
+		height: 4px;
+		border-top: 1px solid white;
+		border-bottom: 1px solid white;
+		width: 16px;
+		margin-left: -8px;
+		opacity: 0.8;
+		content: '';
+	}
+
+	&:hover::after {
+		display: block;
 	}
 }
 </style>
