7ac492df453b397e1b5bcf58da63806d5e2aca87
Mauro Perez <mmauroperez@gmail.com>
2021-05-27 19:31:51 -0400

    del pwa vue ocnfig

--- front/vue.config.js
+++ front/vue.config.js
@@ -2,9 +2,6 @@ const configuration = () => {
 	if (process.env.NODE_ENV === 'production') {
 		return {
 			transpileDependencies: ['vuetify'],
-			pwa: {
-				workboxPluginMode: 'InjectManifest',
-			},
 		};
 	} else {
 		return {
