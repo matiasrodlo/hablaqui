4fc93b4ecd052b024c99ad3624d21584757d7685
Jesus Garcia <fairplaay16@gmail.com>
2021-05-28 09:02:36 -0400

    redirections

--- landing/nuxt.config.js
+++ landing/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 	},
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		titleTemplate: '%s - Hablaqui - Psicología online',
+		titleTemplate: '%s - Psicología online',
 		title: 'Hablaqui',
 		meta: [
 			{ charset: 'utf-8' },
