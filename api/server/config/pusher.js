91118ce78c72226a8495963bc2e7a3bd427eedaa
torokokoo <torokokov7@gmail.com>
2021-06-04 16:13:03 -0400

    refactor pusher config

--- api/server/config/pusher.js
+++ api/server/config/pusher.js
@@ -5,8 +5,6 @@ import {
 	pusher_secret,
 	pusher_cluster,
 } from './dotenv';
-import { pusherCallback } from '../utils/functions/pusherCallback';
-import { logger } from './winston';
 
 const pusher = new Pusher({
 	appId: pusher_app_id,
@@ -16,12 +14,4 @@ const pusher = new Pusher({
 	encrypted: true,
 });
 
-export const pusherTrigger = ({ channel = '', event = '', data = {} }) => {
-	logger.info(
-		`trigger de pusher realizado, canal: ${channel}  evento: ${event} data: ${JSON.stringify(
-			data
-		)}`
-	);
-	return pusher.trigger(channel, event, data, pusherCallback);
-};
 export default pusher;
