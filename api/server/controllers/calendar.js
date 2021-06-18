ed521bb84c27b9b3daa171b58c69d6366fb55149
torokokoo <torokokov7@gmail.com>
2021-06-18 13:57:57 -0400

    sorprendentemente sigue funcionando

---
+++ api/server/controllers/calendar.js
@@ -0,0 +1,27 @@
+import { errorCallback } from '../utils/functions/errorCallback';
+import { restResponse } from '../utils/responses/functions';
+import calendarService from '../services/calendar';
+
+const calendarController = {
+	async getEvents(req, res) {
+		try {
+			const { user } = req;
+			const { data, code } = await calendarService.getEvents(user);
+			return restResponse(data, code, res);
+		} catch (e) {
+			return errorCallback(e, res);
+		}
+	},
+
+	async createEvent(req, res) {
+		try {
+			const { user } = req;
+			const { data, code } = await calendarService.createEvent();
+			return restResponse(data, code, res);
+		} catch (e) {
+			return errorCallback(e, res);
+		}
+	},
+};
+
+export default Object.freeze(calendarController);
