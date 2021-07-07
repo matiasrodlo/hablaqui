10fafc37ebda5227914ef62e3d6e03a3d61cf6ef
Jesus Garcia <fairplaay16@gmail.com>
2021-07-07 10:00:44 -0400

    new components 71

---
+++ landing/pages/psicologos/panquehue.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<Ubicacion />
+	</div>
+</template>
+
+<script>
+export default {
+	components: {
+		Ubicacion: () => import('~/components/psicologos/Ubicacion'),
+	},
+};
+</script>
