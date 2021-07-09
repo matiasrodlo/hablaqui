899f120e088d17168800e5af1ab16a17560d4535
Jesus Garcia <fairplaay16@gmail.com>
2021-07-09 09:50:17 -0400

    add site map

--- landing/nuxt.config.js
+++ landing/nuxt.config.js
@@ -2,19 +2,6 @@ import axios from 'axios';
 
 export default {
 	target: 'static',
-	generate: {
-		fallback: '404.html',
-		routes() {
-			const baseURL = process.env.VUE_APP_URL
-				? process.env.VUE_APP_URL
-				: 'http://localhost:3000/api/v1';
-			return axios.get(`${baseURL}/blog/all`).then(res => {
-				return res.data.articles.map(item => {
-					return { route: `/blog/${item.slug}`, payload: item };
-				});
-			});
-		},
-	},
 	publicRuntimeConfig: {
 		LANDING_URL:
 			process.env.NODE_ENV === 'production'
@@ -37,6 +24,19 @@ export default {
 	},
 	server: {
 		port: process.env.FRONTEND_URL ? 8080 : 9000, // default: 3000
+	},
+	generate: {
+		fallback: '404.html',
+		routes() {
+			const baseURL = process.env.VUE_APP_URL
+				? process.env.VUE_APP_URL
+				: 'http://localhost:3000/api/v1';
+			return axios.get(`${baseURL}/blog/all`).then(res => {
+				return res.data.articles.map(item => {
+					return { route: `/blog/${item.slug}`, payload: item };
+				});
+			});
+		},
 	},
 	loading: {
 		color: '#2070E5',
@@ -102,11 +102,19 @@ export default {
 		// https://go.nuxtjs.dev/axios
 		'@nuxtjs/axios',
 		'@nuxtjs/auth-next',
+		'@nuxtjs/sitemap',
 	],
 
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {
 		baseUrl: process.env.VUE_APP_URL ? process.env.VUE_APP_URL : 'http://localhost:3000/api/v1',
+	},
+
+	sitemap: {
+		hostname: process.env.VUE_APP_LANDING
+			? process.env.VUE_APP_LANDING
+			: 'http://localhost:9000/',
+		exclude: ['/dashboard/**', '/nuevo-psicologo'],
 	},
 
 	auth: {
