6c1f2f80ab85aaa4322267cc1f368558831338a4
Jesus Garcia <fairplaay16@gmail.com>
2021-07-12 15:10:24 -0400

    try  onli client

--- landing/components/psicologos/Ubicacion.vue
+++ landing/components/psicologos/Ubicacion.vue
@@ -3,41 +3,45 @@
 		<!-- appbar -->
 		<appbar />
 		<!-- geo psicologos -->
-		<geoPsicologos :location="location" />
+		<client-only>
+			<geoPsicologos :location="location" />
+		</client-only>
 		<!-- routing for child -->
 		<psicologos />
 		<!-- breadcrubs -->
-		<v-container>
-			<v-row>
-				<v-col cols="12" offset-md="3" md="9">
-					<v-breadcrumbs
-						:items="[
-							{
-								text: 'Página de inicio',
-								disabled: false,
-								href: '/',
-							},
-							{
-								text: 'Psicólogos',
-								disabled: false,
-								href: '/psicologos',
-							},
-							{
-								text: location ? location.name : '',
-								disabled: true,
-								href: `/psicologos/${location ? location.slug : ''}`,
-							},
-						]"
-					>
-						<template #item="{ item }">
-							<v-breadcrumbs-item :href="item.href" :disabled="item.disabled">
-								<span class="body-1 font-weight-medium">{{ item.text }}</span>
-							</v-breadcrumbs-item>
-						</template>
-					</v-breadcrumbs>
-				</v-col>
-			</v-row>
-		</v-container>
+		<client-only>
+			<v-container>
+				<v-row>
+					<v-col cols="12" offset-md="3" md="9">
+						<v-breadcrumbs
+							:items="[
+								{
+									text: 'Página de inicio',
+									disabled: false,
+									href: '/',
+								},
+								{
+									text: 'Psicólogos',
+									disabled: false,
+									href: '/psicologos',
+								},
+								{
+									text: location ? location.name : '',
+									disabled: true,
+									href: `/psicologos/${location ? location.slug : ''}`,
+								},
+							]"
+						>
+							<template #item="{ item }">
+								<v-breadcrumbs-item :href="item.href" :disabled="item.disabled">
+									<span class="body-1 font-weight-medium">{{ item.text }}</span>
+								</v-breadcrumbs-item>
+							</template>
+						</v-breadcrumbs>
+					</v-col>
+				</v-row>
+			</v-container>
+		</client-only>
 		<!-- footer -->
 		<div style="background-color: #0f3860" class="mt-16">
 			<v-container class="white--text py-16">
