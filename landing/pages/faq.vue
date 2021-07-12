613033a7c11dfa2e3a069637355dd82cf107577f
Jesus Garcia <fairplaay16@gmail.com>
2021-07-12 16:10:54 -0400

    fix faq

--- landing/pages/faq.vue
+++ landing/pages/faq.vue
@@ -24,93 +24,97 @@
 				</v-row>
 			</v-container>
 		</div>
-		<v-container v-if="items.length" class="mt-16">
-			<v-row v-if="itemsFilter.length">
-				<v-col cols="12">
-					<div v-for="(item, g) in itemsFilter" :key="g">
-						<div class="text-left text--secondary font-weight-bold title mt-10">
-							{{ item.title }}
-						</div>
-						<div class="caption">
-							{{ item.description }}
-							<div v-for="(d, p) in item.detail" :key="p">
-								<div class="text--secondary font-weight-bold body-1 mt-4">
-									{{ d.title }}
-								</div>
-								<div class="caption">
-									{{ d.description }}
+		<client-only>
+			<v-container v-if="items.length" class="mt-16">
+				<v-row v-if="itemsFilter.length">
+					<v-col cols="12">
+						<div v-for="(item, g) in itemsFilter" :key="g">
+							<div class="text-left text--secondary font-weight-bold title mt-10">
+								{{ item.title }}
+							</div>
+							<div class="caption">
+								{{ item.description }}
+								<div v-for="(d, p) in item.detail" :key="p">
+									<div class="text--secondary font-weight-bold body-1 mt-4">
+										{{ d.title }}
+									</div>
+									<div class="caption">
+										{{ d.description }}
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</v-col>
-			</v-row>
-			<v-row v-else>
-				<v-col cols="12" sm="3">
-					<v-list flat dense>
-						<v-list-item-group v-model="selectedItem" mandatory>
-							<v-list-item v-for="(q, i) in items" :key="i" :value="q">
-								<v-list-item-content>
-									<v-list-item-title
-										class="body-2"
-										:class="
-											selectedItem.id == q.id
-												? 'primary--text'
-												: 'text--secondary'
-										"
-										v-text="q.title"
-									></v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-						</v-list-item-group>
-					</v-list>
-				</v-col>
-				<v-col v-if="selectedItem" cols="12" sm="9" class="pt-6">
-					<div class="primary--text headline font-weight-bold">
-						{{ selectedItem.title }}
-					</div>
-					<v-expansion-panels flat>
-						<v-expansion-panel v-for="(el, i) in selectedItem.faq" :key="i">
-							<v-expansion-panel-header
-								disable-icon-rotate
-								class="body-1 font-weight-bold text--secondary"
-							>
-								{{ el.title }}
-								<template #actions>
-									<v-icon color="info"> mdi-chevron-right </v-icon>
-								</template>
-							</v-expansion-panel-header>
-							<v-expansion-panel-content>
-								<div>
-									{{ el.description }}
-								</div>
-								<template v-if="el.detail">
-									<div v-for="(detail, a) in el.detail" :key="a">
-										<div class="text--secondary font-weight-bold body-1 mt-4">
-											{{ detail.title }}
-										</div>
-										<div class="caption">
-											{{ detail.description }}
-										</div>
+					</v-col>
+				</v-row>
+				<v-row v-else>
+					<v-col cols="12" sm="3">
+						<v-list flat dense>
+							<v-list-item-group v-model="selectedItem" mandatory>
+								<v-list-item v-for="(q, i) in items" :key="i" :value="q">
+									<v-list-item-content>
+										<v-list-item-title
+											class="body-2"
+											:class="
+												selectedItem.id == q.id
+													? 'primary--text'
+													: 'text--secondary'
+											"
+											v-text="q.title"
+										></v-list-item-title>
+									</v-list-item-content>
+								</v-list-item>
+							</v-list-item-group>
+						</v-list>
+					</v-col>
+					<v-col v-if="selectedItem" cols="12" sm="9" class="pt-6">
+						<div class="primary--text headline font-weight-bold">
+							{{ selectedItem.title }}
+						</div>
+						<v-expansion-panels flat>
+							<v-expansion-panel v-for="(el, i) in selectedItem.faq" :key="i">
+								<v-expansion-panel-header
+									disable-icon-rotate
+									class="body-1 font-weight-bold text--secondary"
+								>
+									{{ el.title }}
+									<template #actions>
+										<v-icon color="info"> mdi-chevron-right </v-icon>
+									</template>
+								</v-expansion-panel-header>
+								<v-expansion-panel-content>
+									<div>
+										{{ el.description }}
 									</div>
-								</template>
-							</v-expansion-panel-content>
-							<v-divider style="border-width: 1px"></v-divider>
-						</v-expansion-panel>
-					</v-expansion-panels>
-				</v-col>
-			</v-row>
-		</v-container>
-		<v-container v-else style="height: 200px">
-			<v-row class="fill-height ma-0" align="center" justify="center">
-				<v-progress-circular
-					width="6"
-					size="50"
-					indeterminate
-					color="primary"
-				></v-progress-circular>
-			</v-row>
-		</v-container>
+									<template v-if="el.detail">
+										<div v-for="(detail, a) in el.detail" :key="a">
+											<div
+												class="text--secondary font-weight-bold body-1 mt-4"
+											>
+												{{ detail.title }}
+											</div>
+											<div class="caption">
+												{{ detail.description }}
+											</div>
+										</div>
+									</template>
+								</v-expansion-panel-content>
+								<v-divider style="border-width: 1px"></v-divider>
+							</v-expansion-panel>
+						</v-expansion-panels>
+					</v-col>
+				</v-row>
+			</v-container>
+			<v-container v-else style="height: 200px">
+				<v-row class="fill-height ma-0" align="center" justify="center">
+					<v-progress-circular
+						width="6"
+						size="50"
+						indeterminate
+						color="primary"
+					></v-progress-circular>
+				</v-row>
+			</v-container>
+		</client-only>
 		<div style="background-color: #0f3860; margin-top: 120px; margin-bottom: 100px">
 			<v-container class="white--text py-16">
 				<v-row>
@@ -162,7 +166,9 @@
 				</v-col>
 			</v-row>
 		</v-container>
-		<FloatingChat v-if="$auth.$state.loggedIn && $auth.$state.user.role == 'user'" />
+		<client-only>
+			<FloatingChat v-if="$auth.$state.loggedIn && $auth.$state.user.role == 'user'" />
+		</client-only>
 	</div>
 </template>
 
@@ -174,12 +180,14 @@ export default {
 		Appbar: () => import('@/components/AppbarWhite'),
 		FloatingChat: () => import('@/components/dashboard/FloatingChat'),
 	},
+	async asyncData({ $config }) {
+		let response = await fetch(`${$config.LANDING_URL}/faq.json`);
+		response = await response.json();
+		return { selectedItem: response[0], items: response };
+	},
 	data() {
 		return {
-			selectedItem: null,
-			items: [],
 			search: '',
-			loading: false,
 		};
 	},
 	head() {
@@ -221,14 +229,6 @@ export default {
 	},
 	created() {
 		this.setFloatingChat(false);
-	},
-	async mounted() {
-		this.loading = true;
-		let response = await fetch(`${this.$config.LANDING_URL}/faq.json`);
-		response = await response.json();
-		this.selectedItem = response[0];
-		this.items = response;
-		this.loading = false;
 	},
 	methods: {
 		...mapMutations({
