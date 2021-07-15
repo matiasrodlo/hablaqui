21915d6dc1ccc3976205538338c40817afd0fcd2
torokokoo <torokokov7@gmail.com>
2021-07-15 15:02:53 -0400

    initial commit

---
+++ api/server/models/coupons.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+/* Restrictions example
+/ firstTimeOnly: boolean,
+/ maxUses: number,
+/ psychologistSpecific: objectId,
+*/
+
+let coupon = new Schema({
+	code: {
+		type: String,
+		required: true,
+	},
+	discount: {
+		type: Number,
+		required: true,
+	},
+	discountType: {
+		type: String,
+		enum: ['percentage', 'static'],
+		required: true,
+	},
+	restrictions: {
+		type: Object,
+	},
+	expiration: {
+		type: String,
+	},
+});
+
+export default model('coupon', coupon);
