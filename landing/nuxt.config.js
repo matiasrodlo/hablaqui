8a0ec0deeaaa6235eb03aaf29eef20e431bd7651
Jesus Garcia <fairplaay16@gmail.com>
2021-07-19 15:48:11 -0400

    fonts roboto

--- landing/nuxt.config.js
+++ landing/nuxt.config.js
@@ -153,7 +153,13 @@ export default {
 				content: 'Psicólogo y terapia online de calidad sin salir de casa | Hablaquí',
 			},
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		link: [
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap',
+			},
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+		],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
