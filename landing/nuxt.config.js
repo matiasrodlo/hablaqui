eaab49d45bcad60f4ba6a11c8c1398ad66a349b7
Jesus Garcia <fairplaay16@gmail.com>
2021-07-20 09:10:18 -0400

    rm css mdi

--- landing/nuxt.config.js
+++ landing/nuxt.config.js
@@ -169,11 +169,7 @@ export default {
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [
-		'@mdi/font/css/materialdesignicons.min.css',
-		'vuetify/dist/vuetify.min.css',
-		'~/assets/global.scss',
-	],
+	css: ['vuetify/dist/vuetify.min.css', '~/assets/global.scss'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: ['~/plugins/jsonld'],
