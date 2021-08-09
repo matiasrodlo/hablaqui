bce93b2b647c19aaec4e1f7cb49ef28e6a3454ea
Jesus Garcia <fairplaay16@gmail.com>
2021-08-09 17:14:41 -0400

    1part

--- landing/pages/para-especialistas.vue
+++ landing/pages/para-especialistas.vue
@@ -1,158 +1,106 @@
 <template>
 	<div>
-		<div class="primary-color">
+		<v-img :src="`${$config.LANDING_URL}/especialistas.png`">
 			<Appbar />
-		</div>
-		<v-container fluid class="primary py-16">
-			<v-row justify="center" align="center">
-				<v-col cols="12" md="5" lg="6">
-					<h2 class="text-left white--text text-h4 text-lg-h3 font-weight-bold">
-						Tenga un consultorio sin salir de casa
-					</h2>
-					<div class="mt-3 text-left white--text text-h6 font-weight-bold">
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-						nibh
-					</div>
-					<v-list color="primary" dark class="text-h6">
-						<v-list-item v-for="el in 3" :key="el">
-							<v-list-item-icon> </v-list-item-icon>
-							<v-list-item-content>
-								Lorem ipsum dolor sit amet, consectetuer adipis-
-							</v-list-item-content>
-						</v-list-item>
-					</v-list>
-				</v-col>
-				<v-col cols="12" md="7" lg="6">
-					<v-form>
-						<v-card flat style="border-radius: 15px">
-							<v-card-text>
-								<v-row justify="center">
-									<v-col cols="6" style="border-right: 1px solid #bdbdbd">
-										<div class="title">Faca parte</div>
-										<div class="subtitle-2 my-6">
-											Lorem ipsum dolor sit amet, consectetuer adi-piscing
-											elit, sed diam nonummy nibh diam
-										</div>
-										<v-row no-gutters>
-											<v-col cols="12">
-												<v-text-field
-													label="Nombre completo"
-													outlined
-													dense
-												></v-text-field>
-											</v-col>
-											<v-col cols="12">
-												<v-text-field
-													label="Correo electronico"
-													outlined
-													dense
-												></v-text-field>
-											</v-col>
-											<v-col cols="12">
-												<v-text-field
-													label="senha"
-													outlined
-													dense
-												></v-text-field>
-											</v-col>
-											<v-col cols="6">
-												<v-text-field
-													label="senha"
-													outlined
-													dense
-												></v-text-field>
-											</v-col>
-											<v-col cols="6">
-												<v-text-field
-													label="senha"
-													outlined
-													dense
-												></v-text-field>
-											</v-col>
-										</v-row>
-									</v-col>
-									<v-col cols="6">
-										<div style="height: 60%">
-											<div class="title text-left">
-												Seleccione su especialidad
-											</div>
-											<div class="d-flex justify-space-between">
-												<div
-													v-for="it in 4"
-													:key="it"
-													class="d-inline-block"
-												>
-													<div
-														style="
-															border: 1px solid #bdbdbd;
-															height: 50px;
-															width: 50px;
-															border-radius: 10px;
-														"
-													/>
-													<div class="text-center text--secondary">
-														Psicologo
-													</div>
-												</div>
-											</div>
-											<v-text-field></v-text-field>
-											<v-text-field></v-text-field>
-										</div>
-
-										<div class="mt-4">
-											<v-checkbox dense hide-details class="d-inline-block" />
-											Aceptar
-											<span
-												class="primary--text p-0 caption"
-												style="cursor: pointer"
-												@click="
-													$router.push({ name: 'terminos-y-condiciones' })
-												"
-											>
-												Términos y Condiciones
-											</span>
-											y
-											<span
-												class="primary--text p-0 caption"
-												style="cursor: pointer"
-												@click="$router.push({ name: 'privacidad' })"
-											>
-												politicas de privacidad
-											</span>
-										</div>
-										<v-btn rounded color="primary" block depressed>
-											Enviar solicitud
-										</v-btn>
-									</v-col>
-								</v-row>
-							</v-card-text>
-						</v-card>
-					</v-form>
-				</v-col>
-			</v-row>
-		</v-container>
+			<v-container tag="section" fluid>
+				<v-row justify="center" align="center">
+					<v-col cols="12" md="5">
+						<h1 style="font-size: 60px" class="text-left white--text font-weight-bold">
+							Únete a la red de salud <br />
+							mental y bienestar
+						</h1>
+						<h2 class="mt-3 text-left white--text text-h5 font-weight-bold">
+							Descubre los beneficios de ser parte de Hablaquí y brinda terapia online
+							desde cualquier lugar
+						</h2>
+						<v-list dark color="transparent" class="text-h5">
+							<v-list-item>
+								<v-list-item-icon>
+									<icon color="white" :icon="mdiCheck" />
+								</v-list-item-icon>
+								<v-list-item-content>
+									Oficina virtual práctica y segura.
+								</v-list-item-content>
+							</v-list-item>
+							<v-list-item>
+								<v-list-item-icon>
+									<icon color="white" :icon="mdiCheck" />
+								</v-list-item-icon>
+								<v-list-item-content>
+									Acceso a clientes registrados.
+								</v-list-item-content>
+							</v-list-item>
+							<v-list-item>
+								<v-list-item-icon>
+									<icon color="white" :icon="mdiCheck" />
+								</v-list-item-icon>
+								<v-list-item-content>
+									Crecimiento y desarrollo profesional.
+								</v-list-item-content>
+							</v-list-item>
+							<v-list-item>
+								<v-list-item-icon>
+									<icon color="white" :icon="mdiCheck" />
+								</v-list-item-icon>
+								<v-list-item-content>
+									Tarifas accesibles y convenientes
+								</v-list-item-content>
+							</v-list-item>
+						</v-list>
+					</v-col>
+					<v-col cols="12" md="4">
+						<v-form>
+							<v-card flat class="rounded-lg">
+								<v-card-title>
+									<h3 class="primary--text text-h5">Únete a la red</h3>
+								</v-card-title>
+								<v-card-text class="text--disabled body-1">
+									En solo unos pasos puedes unirte. Comienza creando tu cuenta.
+								</v-card-text>
+								<v-card-text>
+									<v-text-field label="Ingresa rut" outlined></v-text-field>
+								</v-card-text>
+								<v-card-actions>
+									<v-btn color="primary" class="ml-2 px-10">Ok</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-form>
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-img>
 		<v-container>
 			<v-row justify="center">
 				<v-col cols="12">
-					<div class="mt-16 text-center text-h4 text-lg-h3 font-weight-bold">
-						Tenga un consultorio sin salir de casa
+					<div class="mt-16 text-center text-h4 text-lg-h3 font-weight-bold text-color">
+						Tu consulta online personalizada
 					</div>
-					<div class="mt-3 mb-16 text-center text-h6">
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-						nibh
+					<div class="mt-3 mb-16 text-center text-h6 text-color">
+						Cree su propia oficina en minutos con alta tecnología y facilidad
 					</div>
 				</v-col>
-				<v-col v-for="item in 6" :key="item" cols="4">
-					<v-card flat>
+				<v-col v-for="item in 6" :key="item" cols="4" class="my-16">
+					<v-card tag="section" flat>
 						<v-card-text class="text-center">
-							<v-avatar size="150" color="secondary"></v-avatar>
+							<v-img
+								height="113px"
+								width="120px"
+								contain
+								class="mx-auto"
+								:src="`${$config.LANDING_URL}/video-sesion.png`"
+								:lazy-src="`${$config.LANDING_URL}/video-sesion.png`"
+								alt="video sesion"
+							/>
+						</v-card-text>
+						<v-card-text class="text-center pt-0">
+							<h3 class="title font-weight-bold text-h5">
+								Plataforma de video de sesión
+							</h3>
 						</v-card-text>
 						<v-card-text class="text-center">
-							<div class="title">Tenga un consultorio sin</div>
-							<div class="subtitle-2">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nobis
-								facilis. Et quidem laborum, quibusdam vel debitis.
-							</div>
+							<article class="body-1 mx-auto text-h6">
+								En su nueva oficina virtual, atienda a clientes de todo el mundo
+							</article>
 						</v-card-text>
 					</v-card>
 				</v-col>
@@ -268,10 +216,18 @@
 </template>
 
 <script>
+import { mdiCheck } from '@mdi/js';
+
 export default {
 	components: {
 		Footer: () => import('@/components/Footer'),
+		Icon: () => import('~/components/Icon'),
 		Appbar: () => import('@/components/AppbarWhite'),
+	},
+	data() {
+		return {
+			mdiCheck,
+		};
 	},
 	head() {
 		return {
@@ -288,4 +244,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.text-color {
+	color: #575757;
+}
+</style>
