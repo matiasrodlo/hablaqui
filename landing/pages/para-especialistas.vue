165fbf7b51650313a6813640b1f8da1937d4b6eb
Jesus Garcia <fairplaay16@gmail.com>
2021-08-10 09:02:47 -0400

    uploading img

--- landing/pages/para-especialistas.vue
+++ landing/pages/para-especialistas.vue
@@ -2,8 +2,8 @@
 	<div>
 		<v-img :src="`${$config.LANDING_URL}/especialistas.png`">
 			<Appbar />
-			<v-container tag="section" fluid>
-				<v-row justify="center" align="center">
+			<v-container tag="section" fluid style="height: calc(100% - 180px)">
+				<v-row justify="center" align="center" style="height: 100%">
 					<v-col cols="12" md="5">
 						<h1 style="font-size: 60px" class="text-left white--text font-weight-bold">
 							Únete a la red de salud <br />
@@ -79,7 +79,7 @@
 						Cree su propia oficina en minutos con alta tecnología y facilidad
 					</div>
 				</v-col>
-				<v-col v-for="item in 6" :key="item" cols="4" class="my-16">
+				<v-col v-for="item in 6" :key="item" cols="12" md="4" class="my-16">
 					<v-card tag="section" flat>
 						<v-card-text class="text-center">
 							<v-img
@@ -106,112 +106,10 @@
 				</v-col>
 			</v-row>
 		</v-container>
-		<v-img :src="`${$config.LANDING_URL}/wave-blue-1.png`" />
-		<div class="primary">
-			<v-container class="py-16">
-				<v-row justify="center" class="my-10">
-					<v-col cols="3" class="text-right">
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-					</v-col>
-					<v-col cols="4" class="text-center">
-						<v-avatar color="white" size="400"></v-avatar>
-					</v-col>
-					<v-col cols="3" class="text-left">
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-						<v-avatar color="white" size="100" class="mt-10"></v-avatar>
-						<div class="title white--text">Tenga un consultorio sin</div>
-					</v-col>
-				</v-row>
-			</v-container>
-		</div>
-		<v-img :src="`${$config.LANDING_URL}/wave-blue-2.png`" />
-		<v-container>
-			<v-row justify="center">
-				<v-col cols="12">
-					<div class="mt-16 text-center text-h4 text-lg-h3 font-weight-bold">
-						Tenga un consultorio sin salir de casa
-					</div>
-					<div class="mt-3 mb-16 text-center text-h6">
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-						nibh
-					</div>
-				</v-col>
-				<v-col v-for="item in 3" :key="item" cols="4">
-					<v-card flat>
-						<v-card-text class="text-center">
-							<v-avatar size="150" color="secondary"></v-avatar>
-						</v-card-text>
-						<v-card-text class="text-center">
-							<div class="title">Tenga un consultorio sin</div>
-							<div class="subtitle-2">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nobis
-								facilis. Et quidem laborum, quibusdam vel debitis.
-							</div>
-						</v-card-text>
-					</v-card>
-				</v-col>
-				<v-col cols="12" class="text-center">
-					<v-btn color="primary" rounded x-large>Enviar solicitud</v-btn>
-					<div class="my-10 title">Lorem ipsum dolor sit amet</div>
-				</v-col>
-			</v-row>
+		<v-container tag="footer">
+			<Footer />
 		</v-container>
-		<v-img :src="`${$config.LANDING_URL}/wave-blue-1.png`" />
-		<div class="primary">
-			<v-container class="py-16">
-				<v-row align="center" justify="center" class="my-10">
-					<v-col cols="12">
-						<div
-							class="
-								mt-16
-								white--text
-								text-center text-h4 text-lg-h3
-								font-weight-bold
-							"
-						>
-							Tenga un consultorio sin salir de casa
-						</div>
-						<div class="mt-3 white--text mb-16 text-center text-h6">
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-							nonummy nibh
-						</div>
-					</v-col>
-					<v-col cols="5">
-						<v-card flat height="300" style="border-radius: 15px"> </v-card>
-					</v-col>
-					<v-col cols="5">
-						<v-list three-line color="primary" dark>
-							<template v-for="item in 3">
-								<v-list-item :key="item" class="my-10">
-									<v-list-item-avatar
-										size="60"
-										color="white"
-									></v-list-item-avatar>
-									<v-list-item-content>
-										<v-list-item-title class="text-h4">
-											Tenga un consultorio sin
-										</v-list-item-title>
-										<v-list-item-subtitle class="title">
-											Lorem ipsum dolor sit amet, consecte-
-										</v-list-item-subtitle>
-									</v-list-item-content>
-								</v-list-item>
-							</template>
-						</v-list>
-					</v-col>
-				</v-row>
-			</v-container>
-		</div>
-		<v-img :src="`${$config.LANDING_URL}/wave-blue-2.png`" />
-		<Footer />
+		<div class="primary-color" style="height: 30px"></div>
 	</div>
 </template>
 
