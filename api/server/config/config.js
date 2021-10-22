7aa147e835069069136325e47caf285731fc5f55
Jesus Garcia <fairplaay16@gmail.com>
2021-10-22 09:47:32 -0400

    add change

--- api/server/config/config.js
+++ api/server/config/config.js
@@ -4,6 +4,7 @@
 // ============================
 import dotenv from 'dotenv';
 dotenv.config();
+process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/hablaqui';
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.FRONTEND_URL =
