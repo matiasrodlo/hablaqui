0c28b49527bb77e8542d7544be946fde4badd4ea
torokokoo <torokokov7@gmail.com>
2021-11-10 13:17:48 -0300

    documentation

--- api/server/routes/mercadopago.js
+++ api/server/routes/mercadopago.js
@@ -6,7 +6,12 @@ import mercadopagoController from '../controllers/mercadopago';
 const mercadopagoRouter = Router();
 
 /**
- * Crea una preferencia en mercadopago (sirve para extraer un link donde pagar)
+ * @description: Ruta para crear una preferencia (URL de pago) de un plan de usuario
+ * @param {Object} body - Objeto con los parametros necesarios
+ * @param {String} body.descritipn - Titulo del objeto de pago
+ * @param {Integer} body.price - Precio a pagar (en CLP)
+ * @param {String} body.plan - ObjectId del plan creado previamente
+ * @returns: Preferencia, pero lo importante es el init_point (la URL para redireccionar a mercadopago)
  */
 mercadopagoRouter.post(
 	'/mercadopago/create-preference',
@@ -14,18 +19,29 @@ mercadopagoRouter.post(
 );
 
 /**
- * Pasa una sesion a pagada.
+ * @description: Esta ruta no debe ser usada por frontend, solo es back_url de la preferencia.
  */
 mercadopagoRouter.get(
 	'/mercadopago/success-pay/:planId',
 	mercadopagoController.successPay
 );
 
+/**
+ * @description: Ruta para crear una preferencia (URL de pago) de un plan de psicologo
+ * @param {Object} body - Objeto con los parametros necesarios
+ * @param {String} body.descritipn - Titulo del objeto de pago
+ * @param {Integer} body.price - Precio a pagar (en CLP)
+ * @param {String} body.psychologistToUpdate - ObjectId del psicologo que ha pagado
+ * @returns: Preferencia, pero lo importante es el init_point (la URL para redireccionar a mercadopago)
+ */
 mercadopagoRouter.post(
 	'/mercadopago/psychologist-preference',
 	mercadopagoController.createPsychologistPreference
 );
 
+/**
+ * @description: Esta ruta no debe ser usada por frontend, solo es back_url de la preferencia.
+ */
 mercadopagoRouter.get(
 	'/mercadopago/psychologist-pay/:psychologistId/:price',
 	mercadopagoController.psychologistPay
