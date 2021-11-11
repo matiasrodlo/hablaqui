8bd32ddea7608dec43306d617d2d11986ce130c6
Hugo <hugointe@gmail.com>
2021-11-11 16:16:21 -0300

    minor fix

--- landing/components/postulacion/Plans.vue
+++ landing/components/postulacion/Plans.vue
@@ -72,7 +72,7 @@
 							</v-radio>
 						</v-card-text>
 					</v-card>
-					<v-btn class="mt-4" color="primary" rounded block @click="goMercadoPago()"
+					<v-btn class="mt-4" color="primary" rounded block @click="goMercadoPago"
 						>Suscríbete al plan premium
 					</v-btn>
 				</v-radio-group>
