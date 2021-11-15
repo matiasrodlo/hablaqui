e6db4ab9da75ab87f1b1ce90fec52d4b7af7a3e1
Jesus Garcia <fairplaay16@gmail.com>
2021-11-15 16:02:02 -0400

    add timestamp

--- api/server/models/user.js
+++ api/server/models/user.js
@@ -3,69 +3,74 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-let userSchema = new Schema({
-	name: {
-		type: String,
+let userSchema = new Schema(
+	{
+		name: {
+			type: String,
+		},
+		lastName: {
+			type: String,
+		},
+		rut: {
+			type: String,
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+		},
+		inviteCode: {
+			type: String,
+		},
+		googleId: {
+			type: String,
+		},
+		phone: {
+			type: String,
+			trim: true,
+			required: false,
+		},
+		timeZone: {
+			type: String,
+			default: 'America/Santiago',
+		},
+		state: {
+			type: Boolean,
+			default: true,
+		},
+		avatar: { type: String, default: '' },
+		avatarThumbnail: {
+			type: String,
+		},
+		google: {
+			type: Boolean,
+			default: false,
+		},
+		hasPaid: {
+			type: Boolean,
+			default: 'false',
+		},
+		finishedSessions: {
+			type: Array,
+			required: false,
+		},
+		psychologist: {
+			type: Schema.Types.ObjectId,
+			ref: 'psychologist',
+			required: false,
+		},
+		role: {
+			type: String,
+			default: 'user',
+			enum: ['user', 'psychologist', 'superuser'],
+		},
 	},
-	lastName: {
-		type: String,
-	},
-	rut: {
-		type: String,
-	},
-	email: {
-		type: String,
-		lowercase: true,
-		trim: true,
-	},
-	password: {
-		type: String,
-	},
-	inviteCode: {
-		type: String,
-	},
-	googleId: {
-		type: String,
-	},
-	phone: {
-		type: String,
-		trim: true,
-		required: false,
-	},
-	timeZone: {
-		type: String,
-		default: 'America/Santiago',
-	},
-	state: {
-		type: Boolean,
-		default: true,
-	},
-	avatar: { type: String, default: '' },
-	avatarThumbnail: {
-		type: String,
-	},
-	google: {
-		type: Boolean,
-		default: false,
-	},
-	hasPaid: {
-		type: Boolean,
-		default: 'false',
-	},
-	finishedSessions: {
-		type: Array,
-		required: false,
-	},
-	psychologist: {
-		type: Schema.Types.ObjectId,
-		ref: 'psychologist',
-		required: false,
-	},
-	role: {
-		type: String,
-		default: 'user',
-		enum: ['user', 'psychologist', 'superuser'],
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 export default model('User', userSchema);
