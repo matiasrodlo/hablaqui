66cd01f852c9c578e8669f0ea3de4d852fa0c09e
Jesus Garcia <fairplaay16@gmail.com>
2021-12-01 11:05:41 -0400

    payment cards

--- landing/components/dashboard/TablePagos.vue
+++ landing/components/dashboard/TablePagos.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="pb-10">
 		<v-row align="center">
 			<v-col v-if="hideSearch" cols="12" md="4" lg="3">
 				<v-text-field
@@ -53,7 +53,7 @@
 			</v-col>
 		</v-row>
 		<v-data-table
-			:search="search"
+			class="hidden-sm-and-down"
 			:loading="loading"
 			:headers="headers"
 			:items="payments"
@@ -66,6 +66,24 @@
 			no-data-text="No hay pagos"
 		>
 		</v-data-table>
+		<v-card v-for="(item, i) in payments" :key="i" class="hidden-md-and-up my-4">
+			<v-card-text class="pb-0 d-flex justify-space-between">
+				<div>
+					<div class="secondary--text font-weight-bold">{{ item.name }}</div>
+					<div class="caption">Nº Sesión:{{ item.sessionsNumber }}</div>
+				</div>
+				<div>
+					<div class="primary--text">{{ item.plan }}</div>
+				</div>
+			</v-card-text>
+			<v-card-actions>
+				<v-chip color="primary" small> Monto {{ item.amount }} </v-chip>
+				<v-spacer></v-spacer>
+				<v-chip color="primary" small> % Hablaquí {{ item.percentage }} </v-chip>
+				<v-spacer></v-spacer>
+				<v-chip color="primary" small> Monto final {{ item.total }} </v-chip>
+			</v-card-actions>
+		</v-card>
 	</div>
 </template>
 
@@ -111,10 +129,17 @@ export default {
 	computed: {
 		payments: {
 			get() {
-				return this.items.filter(
+				let result = this.items.filter(
 					item =>
 						moment(item.date, 'MM-DD-YYYY HH:mm').format('YYYY-MM') === this.findByDate
 				);
+				if (this.search)
+					result = this.items.filter(
+						item =>
+							// eslint-disable-next-line unicorn/prefer-includes
+							item.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+					);
+				return result;
 			},
 			set(item) {
 				return item;
