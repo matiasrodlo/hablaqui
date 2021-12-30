9731b8af38921c4ddcabf2fdb4c5100d07c69544
Jesus Garcia <fairplaay16@gmail.com>
2021-12-30 10:36:41 -0400

    rm loading

--- landing/components/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<v-progress-circular size="40" indeterminate color="primary"></v-progress-circular>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
