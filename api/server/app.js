a2c2930b55cc22c9fdf4b6fa909a2937ca693961
Nicolas Castillo <ncandresponce@hotmail.com>
2022-02-10 17:11:12 -0300

    change CORS-1.1

--- api/server/app.js
+++ api/server/app.js
@@ -30,7 +30,7 @@ const limiter = rateLimit({
 	max: 1000, // limit each IP to 1000 requests per windowMs
 });
 //limiter solve a brute attack problem in the api, every x time you're only allowed to send x quantity of requests
-const whitelist = ['https://www.google.com/'];
+const whitelist = [process.env.VUE_APP_LANDING];
 let corsOptions = {
 	origin: function(origin, callback) {
 		if (whitelist.indexOf(origin) !== -1) callback(null, true);
