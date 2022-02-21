315b662a04641f778497329f4742f06e47ba9e40
Hugo <hugointe@gmail.com>
2022-02-21 11:45:11 -0300

    rm duplicated tracking

--- api/server/services/recruitment.js
+++ api/server/services/recruitment.js
@@ -29,17 +29,6 @@ const recruitmentService = {
 		if (await Recruitment.exists({ rut: payload.rut })) {
 			return conflictResponse('Este postulante ya está registrado');
 		}
-
-		analytics.track({
-			userId: user._id.toString(),
-			event: 'psy-new-application',
-			properties: {
-				email: user.email,
-				name: user.name,
-				lastName: user.lastName,
-				rut: user.rut,
-			},
-		});
 		const recruited = await Recruitment.create(payload);
 		if (
 			process.env.API_URL.includes('hablaqui.cl') ||
