88454ff45032c767658be72fc2f4fd0a2565bc78
Nicolas Castillo <ncandresponce@hotmail.com>
2022-06-08 00:12:03 -0400

    TECH-513: re-edit about date at email

--- api/server/services/mercadopago.js
+++ api/server/services/mercadopago.js
@@ -210,14 +210,14 @@ const successPay = async params => {
 	await mailService.sendScheduleToUser(
 		user,
 		psy,
-		moment(sessionData.date).format(),
+		moment(sessionData.date, 'MM/DD/YYYY HH:mm'),
 		foundPlan.roomsUrl,
 		`1/${planData.totalSessions}`
 	);
 	await mailService.sendScheduleToPsy(
 		user,
 		psy,
-		moment(sessionData.date).format(),
+		moment(sessionData.date, 'MM/DD/YYYY HH:mm'),
 		foundPlan.roomsUrl,
 		`1/${planData.totalSessions}`
 	);
