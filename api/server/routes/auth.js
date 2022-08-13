744ba889dda8bf855d5bcef9d19d2ca847126937
Nicolas Castillo <ncandresponce@hotmail.com>
2022-08-13 18:16:47 -0400

    doc in auth route

--- api/server/routes/auth.js
+++ api/server/routes/auth.js
@@ -9,15 +9,17 @@ import authSchema from '../schemas/auth';
 const authRouter = Router();
 
 /**
- * Endpoint de autenticacion.
+ * @description: Autenticación del login
+ * @returns: Objeto con token de autenticación y usuario
  */
 authRouter.post(
 	'/auth/login',
 	[validation(authSchema.login, 'body'), passport.authenticate('local')],
 	authController.login
 );
+
 /**
- * Endpoint de logout.
+ * @description: Logout
  */
 authRouter.post('/auth/logout', authController.logout);
 
@@ -45,8 +47,10 @@ authRouter.get(
 );
 
 /**
- * Endpoint de registro.
- * req.body = { email: string, password: string }
+ * @description: Registro de usuario
+ * @param {string} password - Contraseña de registro
+ * @param {string} email - Email de registro
+ * @returns: Objeto con token de autenticación y usuario
  */
 authRouter.post(
 	'/auth/register',
@@ -55,8 +59,8 @@ authRouter.post(
 );
 
 /**
- * Recuperar contraseña
- * req.body = { email: string }
+ * @description: Recuperación de contraseña
+ * @param {string} email - Email de recuperación
  */
 authRouter.get(
 	'/auth/send-password-recover/:email',
@@ -64,16 +68,19 @@ authRouter.get(
 );
 
 /**
- * Cambiar contraseña
- * req.body = { password: string }
+ * @description: Cambio de contraseña
+ * @param {string} password - Nueva contraseña
  */
 authRouter.put(
 	'/auth/user/password',
 	passport.authenticate('jwt'),
 	authController.changeUserPassword
 );
+
 /**
- *
+ * @description: Verificación del correo del usuario
+ * @param {string} id - Identificador único del usuario siendo verificado
+ * @returns: Objeto con token de autenticación y usuario
  */
 authRouter.put(
 	'/auth/user/verification/:id',
