373b24b51b06fe55b6a7624ae974a2a798c3f7db
Sebastián <sebastian.gonzalez.cass@usach.cl>
2022-09-02 17:36:59 -0400

    Se documenta en la carpeta config pino.js y winston.js, además en services auth.js, chat.js, recruitment, y users.js, por último se comenta unas lineas en infoMessages.js ubicado en ./utils/logger

--- api/server/services/users.js
+++ api/server/services/users.js
@@ -1,78 +1,78 @@
 'use strict';
 
-import User from '../models/user';
-import Psychologist from '../models/psychologist';
-import Recruitment from '../models/recruitment';
-import { logInfo } from '../config/winston';
-import bcrypt from 'bcryptjs';
-import servicesAuth from './auth';
-import { actionInfo } from '../utils/logger/infoMessages';
-import { conflictResponse, okResponse } from '../utils/responses/functions';
-import { bucket } from '../config/bucket';
-import mailService from './mail';
-import Sessions from '../models/sessions';
-import Coupon from '../models/coupons';
-import moment from 'moment';
-import { room } from '../config/dotenv';
-import Evaluation from '../models/evaluation';
-import Auth from './auth';
+import User from '../models/user'; // user.js contiene la definición del modelo de usuario para mongodb
+import Psychologist from '../models/psychologist'; // psychologist.js contiene la definición del modelo de psicologo para mongodb
+import Recruitment from '../models/recruitment'; // recruitment.js contiene la definición del modelo de reclutamiento para mongodb
+import { logInfo } from '../config/winston'; // winston.js contiene la configuración de winston para el logging
+import bcrypt from 'bcryptjs'; // bcryptjs es una librería para encriptar contraseñas
+import servicesAuth from './auth'; // auth.js contiene la lógica para la autenticación de usuarios
+import { actionInfo } from '../utils/logger/infoMessages'; // infoMessages.js contiene los mensajes de información para el logging
+import { conflictResponse, okResponse } from '../utils/responses/functions'; // functions.js contiene las funciones para las respuestas
+import { bucket } from '../config/bucket'; // bucket.js contiene la configuración de la conexión con el bucket de google cloud storage
+import mailService from './mail'; // mail.js contiene la lógica para el envío de correos electrónicos
+import Sessions from '../models/sessions'; // sessions.js contiene la definición del modelo de sesiones para mongodb
+import Coupon from '../models/coupons'; // coupons.js contiene la definición del modelo de cupones para mongodb
+import moment from 'moment'; // moment.js es una librería para el manejo de fechas
+import { room } from '../config/dotenv'; // dotenv.js contiene la configuración de las variables de entorno
+import Evaluation from '../models/evaluation'; // evaluation.js contiene la definición del modelo de evaluaciones para mongodb
+import Auth from './auth'; // auth.js contiene la lógica para la autenticación de usuarios
 var Analytics = require('analytics-node');
 var analytics = new Analytics(process.env.SEGMENT_API_KEY);
 moment.tz.setDefault('America/Santiago');
 
-const usersService = {
-	async getProfile(id) {
-		const user = await User.findById(id);
-		if (!user) {
-			return conflictResponse('perfil no encontrado');
+const usersService = { // usersService contiene la lógica para los servicios de usuarios
+	async getProfile(id) { // getProfile obtiene el perfil de un usuario
+		const user = await User.findById(id); // Se busca el usuario en la base de datos
+		if (!user) { // Si no se encuentra el usuario
+			return conflictResponse('perfil no encontrado'); // Se retorna un error
 		}
-		return okResponse('perfil obtenido', {
-			user: await servicesAuth.generateUser(user),
+		return okResponse('perfil obtenido', { // Se retorna el perfil del usuario
+			user: await servicesAuth.generateUser(user), // Se genera el perfil del usuario
 		});
 	},
-	async changeActualPassword(user, newPassword) {
-		user.password = bcrypt.hashSync(newPassword, 10);
-		await user.save();
-		logInfo(actionInfo(user.email, 'actualizo su contraseña'));
-		return okResponse('Actualizó su contraseña');
+	async changeActualPassword(user, newPassword) { // changeActualPassword cambia la contraseña actual de un usuario
+		user.password = bcrypt.hashSync(newPassword, 10); // Se encripta la nueva contraseña
+		await user.save(); // Se guarda el usuario en la base de datos
+		logInfo(actionInfo(user.email, 'actualizo su contraseña')); // Se registra el evento en el log
+		return okResponse('Actualizó su contraseña'); // Se retorna un mensaje de éxito
 	},
-	async updatePassword(user, oldPassword, newPassword) {
-		const foundUser = await User.findById(user._id);
-		// if the password is te same we cancel the update
-		const samePassword = oldPassword === newPassword;
-		if (samePassword)
-			return conflictResponse('no puede ser la misma contraseña');
-		const isEqual = bcrypt.compareSync(oldPassword, foundUser.password);
+	async updatePassword(user, oldPassword, newPassword) { // updatePassword actualiza la contraseña de un usuario
+		const foundUser = await User.findById(user._id); // Se busca el usuario en la base de datos
+		// if the password is te same we cancel the update 	// Si la contraseña es la misma se cancela la actualización
+		const samePassword = oldPassword === newPassword; // Se verifica si la contraseña es la misma
+		if (samePassword) // Si la contraseña es la misma
+			return conflictResponse('no puede ser la misma contraseña'); // Se retorna un error
+		const isEqual = bcrypt.compareSync(oldPassword, foundUser.password); // Se verifica si la contraseña es correcta
 		//if the password doesn't match, we cancel the update
-		if (!isEqual)
-			return conflictResponse('la contraseña anterior no es correcta');
-		else return await this.changeActualPassword(foundUser, newPassword);
+		if (!isEqual) // Si la contraseña no es correcta
+			return conflictResponse('la contraseña anterior no es correcta'); // Se retorna un error
+		else return await this.changeActualPassword(foundUser, newPassword); // Se actualiza la contraseña
 	},
-	async passwordRecovery(user, newPassword) {
-		const foundUser = await User.findById(user._id);
-		const isEqual = bcrypt.compareSync(newPassword, foundUser.password);
+	async passwordRecovery(user, newPassword) { // passwordRecovery recupera la contraseña de un usuario
+		const foundUser = await User.findById(user._id); // Se busca el usuario en la base de datos
+		const isEqual = bcrypt.compareSync(newPassword, foundUser.password); // Se verifica si la contraseña es correcta
 		//if the password is the same, we cancel the update with this
-		if (isEqual)
-			return conflictResponse('no puede ser la misma contraseña');
-		else return await this.changeActualPassword(foundUser, newPassword);
+		if (isEqual) // Si la contraseña es la misma
+			return conflictResponse('no puede ser la misma contraseña'); // Se retorna un error
+		else return await this.changeActualPassword(foundUser, newPassword); // Se actualiza la contraseña
 	},
-	async updateProfile(id, profile) {
-		const updated = await User.findByIdAndUpdate(id, profile, {
-			new: true,
-			runValidators: true,
+	async updateProfile(id, profile) { // updateProfile actualiza el perfil de un usuario
+		const updated = await User.findByIdAndUpdate(id, profile, { // Se actualiza el perfil del usuario
+			new: true, 
+			runValidators: true, 
 			context: 'query',
 		});
 
-		return okResponse('Actualizado exitosamente', {
-			user: await servicesAuth.generateUser(updated),
+		return okResponse('Actualizado exitosamente', { // Se retorna un mensaje de éxito
+			user: await servicesAuth.generateUser(updated), // Se genera el perfil del usuario
 		});
 	},
 
-	async updatePlan(user, newPlan) {
-		let updated = null;
-		updated = await User.findByIdAndUpdate(
-			user._id,
-			{ myPlan: newPlan },
+	async updatePlan(user, newPlan) { // updatePlan actualiza el plan de un usuario
+		let updated = null; // Se inicializa la variable updated
+		updated = await User.findByIdAndUpdate( // Se actualiza el plan del usuario
+			user._id, // Se busca el usuario por su id
+			{ myPlan: newPlan }, // Se actualiza el plan del usuario
 			{
 				new: true,
 				runValidators: true,
@@ -80,68 +80,68 @@ const usersService = {
 			}
 		);
 
-		logInfo(actionInfo(user.email, 'actualizo su plan'));
-		return okResponse('plan actualizado', { profile: updated });
+		logInfo(actionInfo(user.email, 'actualizo su plan')); // Se registra el evento en el log
+		return okResponse('plan actualizado', { profile: updated }); // Se retorna un mensaje de éxito
 	},
 
-	async updatePsychologist(user, newPsychologist) {
-		let updated = null;
-		updated = await User.findByIdAndUpdate(
-			user._id,
-			{ psychologist: newPsychologist },
-			{
-				new: true,
-				runValidators: true,
-				context: 'query',
+	async updatePsychologist(user, newPsychologist) { // updatePsychologist actualiza el psicólogo de un usuario
+		let updated = null; // Se inicializa la variable updated
+		updated = await User.findByIdAndUpdate( // Se actualiza el psicólogo del usuario
+			user._id, // Se busca el usuario por su id
+			{ psychologist: newPsychologist }, // Se actualiza el psicólogo del usuario
+			{ 
+				new: true, // que sea new quiere decir que se retorna el objeto actualizado
+				runValidators: true, // valida los datos de entrada, en este caso, el id del psicólogo
+				context: 'query', // query es el contexto de la actualización, el contexto es el tipo de operación que se realiza
 			}
 		);
 
-		logInfo(actionInfo(user.email, 'actualizo su psicologo'));
-		return okResponse('psicologo actualizado', { profile: updated });
+		logInfo(actionInfo(user.email, 'actualizo su psicologo')); // Se registra el evento en el log
+		return okResponse('psicologo actualizado', { profile: updated }); // Se retorna un mensaje de éxito
 	},
 
-	async uploadAvatar({
-		userLogged,
-		avatar,
-		avatarThumbnail,
-		role,
-		idPsychologist,
-		_id,
-		oldAvatar,
-		oldAvatarThumbnail,
+	async uploadAvatar({ // uploadAvatar sube el avatar de un usuario
+		userLogged, // usuario que está logueado
+		avatar, // avatar del usuario
+		avatarThumbnail, // avatar del usuario en miniatura
+		role, // rol del usuario
+		idPsychologist, // id del psicólogo
+		_id, // id del usuario
+		oldAvatar, // avatar anterior del usuario
+		oldAvatarThumbnail, // avatar anterior del usuario en miniatura
 	}) {
-		let psychologist;
-		let userRole = role;
-		let userID = _id;
+		let psychologist; // Se declara la variable psychologist
+		let userRole = role; // Se inicializa la variable userRole
+		let userID = _id; // Se inicializa la variable userID
 
-		if (!avatar && !avatarThumbnail)
-			return conflictResponse('Ha ocurrido un error inesperado');
+		if (!avatar && !avatarThumbnail) // Si no hay avatar ni avatar en miniatura
+			return conflictResponse('Ha ocurrido un error inesperado'); // Se retorna un error
 
-		if (userLogged.role === 'superuser') {
-			const psy = await Psychologist.findById(idPsychologist);
-			const userSelected = await User.findOne({
-				email: psy.email,
-				role: 'psychologist',
+		if (userLogged.role === 'superuser') { // Si el usuario logueado es un superusuario
+			const psy = await Psychologist.findById(idPsychologist); // Se busca el psicólogo en la base de datos
+			const userSelected = await User.findOne({ // Se busca el usuario en la base de datos
+				email: psy.email, // Se busca el usuario por su email
+				role: 'psychologist', // Se busca el usuario por su rol
 			});
-			userRole = userSelected.role;
-			userID = userSelected._id;
+			userRole = userSelected.role; // Se asigna el rol del usuario
+			userID = userSelected._id; // Se asigna el id del usuario
 		}
 
-		if (userRole === 'psychologist') {
-			const userData = await User.findById(userID);
-			await mailService.sendUploadPicture(userData);
-			if (userData.psychologist) {
-				psychologist = await Psychologist.findByIdAndUpdate(
-					idPsychologist,
+		if (userRole === 'psychologist') { // Si el rol del usuario es psicólogo
+			const userData = await User.findById(userID); // Se busca el usuario en la base de datos
+			await mailService.sendUploadPicture(userData); // Se sube la foto del usuario
+			if (userData.psychologist) { // Si el usuario tiene un psicólogo
+				psychologist = await Psychologist.findByIdAndUpdate( // Se encuentra por ID y se actualiza el avatar del psicólogo
+					idPsychologist, // Se busca el psicólogo por su id
 					{
-						avatar,
-						avatarThumbnail,
-						approveAvatar: false,
+						avatar, // Se actualiza el avatar del psicólogo
+						avatarThumbnail, // Se actualiza el avatar del psicólogo en miniatura
+						approveAvatar: false, // Se actualiza el estado de aprobación del avatar del psicólogo
 					},
-					{ new: true }
+					{ new: true } // que sea new quiere decir que se retorna el objeto actualizado
 				);
-			} else {
-				psychologist = await Recruitment.findByIdAndUpdate(
+			} else { // Si el usuario no tiene un psicólogo
+				psychologist = await Recruitment.findByIdAndUpdate( // Se encuentra por ID y se actualiza el avatar del psicólogo
 					userID,
 					{
 						avatar,
@@ -153,7 +153,7 @@ const usersService = {
 			}
 		}
 
-		const profile = await User.findByIdAndUpdate(
+		const profile = await User.findByIdAndUpdate( // Se encuentra por ID y se actualiza el avatar del usuario
 			userID,
 			{
 				avatar,
@@ -165,49 +165,49 @@ const usersService = {
 		);
 
 		// delete old image
-		await this.deleteFile(oldAvatar, oldAvatarThumbnail).catch(
+		await this.deleteFile(oldAvatar, oldAvatarThumbnail).catch( // Se elimina el avatar anterior del usuario
 			console.error
 		);
 
-		logInfo(`${userLogged.email} actualizo su avatar`);
+		logInfo(`${userLogged.email} actualizo su avatar`); // Se registra el evento en el log
 
-		return okResponse('Avatar actualizado', {
+		return okResponse('Avatar actualizado', { // Se retorna un mensaje de éxito
 			user: profile,
 			psychologist,
 		});
 	},
 
-	async deleteFile(oldAvatar, oldAvatarThumbnail) {
-		if (oldAvatar)
-			await bucket
-				.file(oldAvatar.split('https://cdn.hablaqui.cl/').join(''))
-				.delete();
-		if (oldAvatarThumbnail)
-			await bucket
-				.file(
-					oldAvatarThumbnail
-						.split('https://cdn.hablaqui.cl/')
-						.join('')
+	async deleteFile(oldAvatar, oldAvatarThumbnail) { // deleteFile elimina el avatar anterior del usuario
+		if (oldAvatar) // Si hay un avatar anterior
+			await bucket // Se elimina el avatar anterior del usuario
+				.file(oldAvatar.split('https://cdn.hablaqui.cl/').join('')) // Se elimina el avatar anterior del usuario
+				.delete(); // Se elimina el avatar anterior del usuario
+		if (oldAvatarThumbnail) // Si hay un avatar anterior en miniatura
+			await bucket // Se elimina el avatar anterior del usuario en miniatura
+				.file( // Se elimina el avatar anterior del usuario en miniatura
+					oldAvatarThumbnail // Se elimina el avatar anterior del usuario en miniatura
+						.split('https://cdn.hablaqui.cl/') // Se elimina el avatar anterior del usuario en miniatura
+						.join('') // Se elimina el avatar anterior del usuario en miniatura
 				)
-				.delete();
+				.delete(); // Se elimina el avatar anterior del usuario en miniatura
 	},
 
-	async setUserOnline(user) {
+	async setUserOnline(user) { // setUserOnline establece el estado de un usuario como en línea
 		// const data = {
 		// 	...user,
 		// 	status: true,
 		// };
 		// pusher.trigger('user-status', 'online', data, pusherCallback);
-		return okResponse('Usuario conectado', user);
+		return okResponse('Usuario conectado', user); // Se retorna un mensaje de éxito
 	},
 
-	async setUserOffline(user) {
+	async setUserOffline(user) { // setUserOffline establece el estado de un usuario como desconectado
 		// const data = {
 		// 	...user,
 		// 	status: false,
 		// };
 		// pusher.trigger('user-status', 'offline', data, pusherCallback);
-		return okResponse('Usuario desconectado', user);
+		return okResponse('Usuario desconectado', user); // Se retorna un mensaje de éxito
 	},
 
 	async registerUser(user, body) {
