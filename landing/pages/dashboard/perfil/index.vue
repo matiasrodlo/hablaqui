967f6ef4b147437179663a12785bb94c2bf790ce
Sebastián <sebastian.gonzalez.cass@usach.cl>
2022-11-26 22:34:23 -0300

    fix: perfil index.vue

--- landing/pages/dashboard/perfil/index.vue
+++ landing/pages/dashboard/perfil/index.vue
@@ -267,6 +267,12 @@
 <script>
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 import { mdiCamera } from '@mdi/js';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('America/Santiago');
 
 export default {
 	components: {
