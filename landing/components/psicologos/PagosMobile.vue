e35486135b7fd874a4ad23d50e5d1dad2fea0b63
Matías <m@hablaqui.com>
2022-12-04 19:58:30 -0300

    fix: subsciption naming

--- landing/components/psicologos/PagosMobile.vue
+++ landing/components/psicologos/PagosMobile.vue
@@ -236,7 +236,7 @@ export default {
 			itemsPlan: [
 				{
 					id: 1,
-					title: 'Semanal',
+					title: 'Pago semanal',
 					pricePerSession: '$50.000 / por sesión',
 					valuePerSession: '$50.000',
 					priceTotal: '',
@@ -245,7 +245,7 @@ export default {
 				},
 				{
 					id: 2,
-					title: 'Mensual',
+					title: 'Pago mensual',
 					pricePerSession: '$45.000 / por sesión',
 					valuePerSession: '$45.000',
 					priceTotal: '($180000)',
@@ -254,7 +254,7 @@ export default {
 				},
 				{
 					id: 3,
-					title: 'Trimestral',
+					title: 'Pago trimestral',
 					pricePerSession: '$40.000 / por sesión',
 					valuePerSession: '$40.000',
 					priceTotal: '($480000)',
