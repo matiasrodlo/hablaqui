2fb15eed4b0979b00a516f63726f561ee11d784f
Sebastián <sebastian.gonzalez.cass@usach.cl>
2022-12-16 01:23:51 +0000

    feat: reminderChat 1 hour ago

--- api/server/routes/cron.js
+++ api/server/routes/cron.js
@@ -87,4 +87,12 @@ cronRouter.post(
  */
 cronRouter.post('/cron/reminder-chat/:authToken', cronController.reminderChat);
 
+/**
+ * @description: Envia el correo de recordatorio de chat al usuario
+ * @method POST
+ * @route /api/v1/cron/reminder-chat/:authToken
+ * @param {string} params.authToken - Token de autorización
+ */
+cronRouter.post('/cron/reminder-chat/:authToken', cronController.reminderChat);
+
 export default cronRouter;
