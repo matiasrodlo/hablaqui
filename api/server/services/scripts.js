7f9c6bec87717186df264635de92dd29d314bcab
Sebastián <sebastian.gonzalez.cass@usach.cl>
2022-12-26 22:03:46 +0000

    feat: script add-profesion is created

--- api/server/services/scripts.js
+++ api/server/services/scripts.js
@@ -2,10 +2,13 @@
 
 import { okResponse, conflictResponse } from '../utils/responses/functions';
 import userModel from '../models/user';
+import psyModel from '../models/psychologist';
 
 const changeRole = async () => {
+	// Se buca a todos los usuarios con el rol de psicologo
 	const user = await userModel.find({ role: 'psychologist' });
 	if (!user) return conflictResponse('No se encontro ningun usuario');
+	// Se cambia el rol de psicologo a especialista
 	await userModel.updateMany(
 		{ role: 'psychologist' },
 		{ role: 'specialist' }
@@ -13,8 +16,36 @@ const changeRole = async () => {
 	return okResponse('Rol cambiado', { user });
 };
 
+const addProfesion = async () => {
+	// Se busca a todos los usuarios con el rol de psicologo que no tengan el atributo profesion
+	const users = await userModel.find({
+		role: 'psychologist',
+		profesion: { $exists: false },
+	});
+	const psychologists = await psyModel.find({
+		profesion: { $exists: false },
+	});
+	if (!users) return conflictResponse('No se encontro ningun usuario');
+	if (!psychologists)
+		return conflictResponse('No se encontro ningun psicologo');
+	// Se obtienen los id de los usuarios y psicologos
+	const psy = psychologists.map(psy => psy._id);
+	const user = users.map(user => user._id);
+	// Se agrega la profesion a los psicologos
+	await psyModel.updateMany(
+		{ _id: { $in: psy } },
+		{ $set: { profesion: 'Psychologist' } }
+	);
+	await userModel.updateMany(
+		{ _id: { $in: user } },
+		{ $set: { profesion: 'Psychologist' } }
+	);
+	return okResponse('Profesion agregada', { user });
+};
+
 const scriptsService = {
 	changeRole,
+	addProfesion,
 };
 
 export default Object.freeze(scriptsService);
