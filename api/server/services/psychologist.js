4fb176f142b607d5362f16618c2ed8aaecf65dc9
Sebastián <sebastian.gonzalez.cass@usach.cl>
2023-01-13 22:21:02 -0300

    fix: ! is removed

--- api/server/services/psychologist.js
+++ api/server/services/psychologist.js
@@ -527,7 +527,7 @@ const updatePsychologist = async (user, profile) => {
 			);
 			// Hace el trackeo de segment
 			if (
-				!process.env.API_URL.includes('hablaqui.cl') ||
+				process.env.API_URL.includes('hablaqui.cl') ||
 				process.env.DEBUG_ANALYTICS === 'true'
 			) {
 				const getUser = await User.findOne({ email: user.email });
@@ -590,7 +590,7 @@ const updatePsychologist = async (user, profile) => {
 			);
 			// Se hace el trackeo de segment
 			if (
-				!process.env.API_URL.includes('hablaqui.cl') ||
+				process.env.API_URL.includes('hablaqui.cl') ||
 				process.env.DEBUG_ANALYTICS === 'true'
 			) {
 				analytics.track({
