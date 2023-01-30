f1ece6c80bac0731318822c68f185f412464b55a
Fabián Lizama <fabian.lizama@usach.cl>
2023-01-30 17:27:46 -0300

    refactor: filter improvement on sessions data table

--- landing/pages/dashboard/panel.vue
+++ landing/pages/dashboard/panel.vue
@@ -736,6 +736,7 @@
 										:items="filteredSessions"
 										item-key="name"
 										:items-per-page="5"
+										:search="userFilterText"
 									>
 									</v-data-table>
 								</v-card-text>
