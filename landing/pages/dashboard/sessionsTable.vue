d81cef32f2a1589000e127f6a53f4ad4461f8560
Fabián Lizama <fabian.lizama@usach.cl>
2023-02-24 02:56:20 -0300

    fix: datePicker sessions filter fixed

--- landing/pages/dashboard/sessionsTable.vue
+++ landing/pages/dashboard/sessionsTable.vue
@@ -9,66 +9,111 @@
 							<v-row>
 								<v-col>
 									<v-menu
-										ref="menu"
-										v-model="menu"
+										ref="startMenu"
+										v-model="startMenu"
 										:close-on-content-click="false"
-										:return-value.sync="date"
+										:return-value.sync="startDate"
 										transition="scale-transition"
 										offset-y
 										min-width="auto"
 									>
-										<template v-slot:activator="{ on, attrs }">
-										<v-text-field
-											v-model="date"
-											label="Filtro por Fecha"
-											readonly
-											v-bind="attrs"
-											v-on="on"
-										></v-text-field>
+										<template #activator="{ on, attrs }">
+											<v-text-field
+												v-model="startDate"
+												label="Desde"
+												readonly
+												v-bind="attrs"
+												v-on="on"
+											></v-text-field>
 										</template>
-										<v-date-picker
-										v-model="date"
-										range
-										>
-										<v-spacer></v-spacer>
-										<v-btn
-											text
-											color="primary"
-											@click="menu = false"
-										>
-											Cancel
-										</v-btn>
-										<v-btn
-											text
-											color="primary"
-											@click="$refs.menu.save(date)"
-										>
-											OK
-										</v-btn>
+										<v-date-picker v-model="startDate">
+											<v-spacer></v-spacer>
+											<v-btn
+												text
+												color="primary"
+												@click="() => (endMenu = false)"
+											>
+												Cancelar
+											</v-btn>
+											<v-btn
+												text
+												color="primary"
+												@click="$refs.startMenu.save(startDate)"
+											>
+												OK
+											</v-btn>
 										</v-date-picker>
 									</v-menu>
 								</v-col>
 								<v-col>
-								<v-select
-									v-model="statFilterText"
-									:item-value="status.value"
-									:item-text="status.text"
-									:items="status"
-									label="Estado de Realización">
-								</v-select>
+									<v-menu
+										ref="endMenu"
+										v-model="endMenu"
+										:close-on-content-click="false"
+										:return-value.sync="endDate"
+										transition="scale-transition"
+										offset-y
+										min-width="auto"
+									>
+										<template #activator="{ on, attrs }">
+											<v-text-field
+												v-model="endDate"
+												label="Hasta"
+												readonly
+												v-bind="attrs"
+												v-on="on"
+											></v-text-field>
+										</template>
+										<v-date-picker v-model="endDate">
+											<v-spacer></v-spacer>
+											<v-btn
+												text
+												color="primary"
+												@click="() => (endMenu = false)"
+											>
+												Cancelar
+											</v-btn>
+											<v-btn
+												text
+												color="primary"
+												@click="$refs.endMenu.save(endDate)"
+											>
+												OK
+											</v-btn>
+										</v-date-picker>
+									</v-menu>
 								</v-col>
 								<v-col>
-								<v-text-field v-model="psyFilterText"
-												label="Filtro por Especialista"></v-text-field>
+									<v-select
+										v-model="statFilterText"
+										:item-value="status.value"
+										:item-text="status.text"
+										:items="status"
+										label="Estado de Realización"
+									>
+									</v-select>
 								</v-col>
 								<v-col>
-								<v-text-field v-model="userFilterText"
-												label="Filtro por Usuario"></v-text-field>
+									<v-text-field
+										v-model="psyFilterText"
+										label="Filtro por Especialista"
+									></v-text-field>
 								</v-col>
 								<v-col>
-								<v-select v-model="payFilterText" :item-value="payStatus.value" :item-text="payStatus.text"
-											:items="payStatus" label="Estado de Pago">
-								</v-select>
+									<v-text-field
+										v-model="userFilterText"
+										label="Filtro por Usuario"
+									></v-text-field>
+								</v-col>
+								<v-col>
+									<v-select
+										v-model="payFilterText"
+										:item-value="payStatus.value"
+										:item-text="payStatus.text"
+										:items="payStatus"
+										label="Estado de Pago"
+									>
+									</v-select>
 								</v-col>
 							</v-row>
 						</v-card-title>
@@ -92,37 +137,34 @@
 						<v-card-title>
 							<v-row>
 								<v-col>
-									<v-text-field 
+									<v-text-field
 										v-model="nameFilterText"
 										label="Nombre"
 									></v-text-field>
 								</v-col>
 								<v-col>
-									<v-text-field 
+									<v-text-field
 										v-model="lastNameFilterText"
 										label="Apellido"
 									></v-text-field>
 								</v-col>
 								<v-col>
-									<v-text-field 
+									<v-text-field
 										v-model="mailFilterText"
 										label="Correo"
 									></v-text-field>
 								</v-col>
 								<v-col>
-									<v-text-field 
+									<v-text-field
 										v-model="phoneFilterText"
 										label="Teléfono"
 									></v-text-field>
 								</v-col>
 								<v-col>
-									<v-text-field 
-										v-model="idFilterText"
-										label="ID"
-									></v-text-field>
+									<v-text-field v-model="idFilterText" label="ID"></v-text-field>
 								</v-col>
 								<v-col>
-									<v-text-field 
+									<v-text-field
 										v-model="rutFilterText"
 										label="Rut"
 									></v-text-field>
@@ -165,9 +207,7 @@
 				<v-data-table :headers="headersUserSessions" :items="userSessions" />
 			</v-card>
 		</v-dialog>
-
 	</v-container>
-
 </template>
 <script>
 import axios from 'axios';
@@ -177,10 +217,12 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import isBetween from 'dayjs/plugin/isBetween';
 import timezone from 'dayjs/plugin/timezone';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import evaluateErrorReturn from '@/utils/errors/evaluateErrorReturn';
 dayjs.extend(isBetween);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 dayjs.tz.setDefault('America/Santiago');
 
 export default {
@@ -198,9 +240,12 @@ export default {
 			transactions: [],
 			users: [],
 			menu: '',
-			date: '',
+			startDate: '',
+			endDate: '',
 			label: 'Sesiones a pagar',
 			dialog: false,
+			endMenu: false,
+			startMenu: false,
 			showBank: false,
 			paymentMethods: {},
 			dateFilterText: null,
@@ -226,13 +271,13 @@ export default {
 				{ text: 'Estado de Pago', value: 'paymentPlan' },
 			],
 			usersHeaders: [
-				{text: 'Nombre', value: 'name'},
-				{text: 'Apellido', value: 'lastName'},
-				{text: 'Correo', value: 'email'},
-				{text: 'Teléfono', value: 'phone'},
-				{text: 'ID', value: '_id'},
-				{text: 'Rut', value: 'rut'},
-				{text: 'Acciones', value: 'action', sortable: false},
+				{ text: 'Nombre', value: 'name' },
+				{ text: 'Apellido', value: 'lastName' },
+				{ text: 'Correo', value: 'email' },
+				{ text: 'Teléfono', value: 'phone' },
+				{ text: 'ID', value: '_id' },
+				{ text: 'Rut', value: 'rut' },
+				{ text: 'Acciones', value: 'action', sortable: false },
 			],
 			headersUserSessions: [
 				{ text: 'Fecha', value: 'date' },
@@ -245,11 +290,11 @@ export default {
 			sessions: [],
 			status: [
 				{ text: 'Pendiente', value: 'pending' },
-				{ text: 'Realizada', value: 'success' }
+				{ text: 'Realizada', value: 'success' },
 			],
 			payStatus: [
 				{ text: 'Pendiente', value: 'pending' },
-				{ text: 'Pagada', value: 'success' }
+				{ text: 'Pagada', value: 'success' },
 			],
 		};
 	},
@@ -268,13 +313,11 @@ export default {
 			return transactions;
 		},
 		filteredSessions() {
-			let start = []
-			let end = []
-			if (this.date && this.date[0] && this.date[1]){
-				start = this.date[0].split('-')
-				end = this.date[1].split('-')
-			}
-
+			console.log(this.startDate, this.endDate);
+			console.log(dayjs(this.startDate, 'YYYY-MM-DD'), dayjs(this.endDate, 'YYYY-MM-DD'));
+			this.sessions.forEach(session => {
+				console.log(dayjs(session.date, 'DD/MM/YYYY HH:mm'));
+			});
 			// Método que filtra las sesiones según 5 condiciones, nombre de usuario, estatus de la sesión, nombre del psicólogo, fecha de la sesión y estado de pago
 			return this.sessions.filter(
 				session =>
@@ -282,36 +325,47 @@ export default {
 					session.statusSession.includes(this.statFilterText) &&
 					session.specialist.toLowerCase().includes(this.psyFilterText.toLowerCase()) &&
 					session.paymentPlan.includes(this.payFilterText) &&
-					(this.date && this.date[0] && this.date[1]
-						? dayjs(dayjs(session.date).format('DD/MM/YYYY HH:mm')).isBetween(
-						`${start[0]}-${start[1]}-${start[2]}`,
-						`${end[0]}-${end[1]}-${end[2]}`
-					)
-						: true
-					)
+					(this.startDate && this.endDate
+						? dayjs(dayjs(session.date, 'DD/MM/YYYY HH:mm')).isBetween(
+								dayjs(this.startDate, 'YYYY-MM-DD'),
+								dayjs(this.endDate, 'YYYY-MM-DD')
+						  )
+						: true)
 			);
 		},
 		filteredUsers() {
 			return this.users.filter(
 				//  correo telefono id rut
 				user =>
-					(this.nameFilterText === '' ? true : 
-						user.name === undefined ? false :
-							user.name.toLowerCase().includes(this.nameFilterText.toLowerCase())) &&
-					(this.lastNameFilterText === '' ? true :
-						user.lastName === undefined ? false :
-							user.lastName.toLowerCase().includes(this.lastNameFilterText.toLowerCase())) &&
-					(this.mailFilterText === '' ? true :
-						user.email === undefined ? false :
-							user.email.toLowerCase().includes(this.mailFilterText.toLowerCase())) &&
-					(this.phoneFilterText === '' ? true :
-						user.phone === undefined ? false :
-							user.phone.toLowerCase().includes(this.phoneFilterText.toLowerCase())) &&
+					(this.nameFilterText === ''
+						? true
+						: user.name === undefined
+						? false
+						: user.name.toLowerCase().includes(this.nameFilterText.toLowerCase())) &&
+					(this.lastNameFilterText === ''
+						? true
+						: user.lastName === undefined
+						? false
+						: user.lastName
+								.toLowerCase()
+								.includes(this.lastNameFilterText.toLowerCase())) &&
+					(this.mailFilterText === ''
+						? true
+						: user.email === undefined
+						? false
+						: user.email.toLowerCase().includes(this.mailFilterText.toLowerCase())) &&
+					(this.phoneFilterText === ''
+						? true
+						: user.phone === undefined
+						? false
+						: user.phone.toLowerCase().includes(this.phoneFilterText.toLowerCase())) &&
 					user._id.includes(this.idFilterText.toLowerCase()) &&
-					(this.rutFilterText === '' ? true :
-						user.rut === undefined ? false :
-							user.rut.includes(this.rutFilterText.toLowerCase()))
-			)
+					(this.rutFilterText === ''
+						? true
+						: user.rut === undefined
+						? false
+						: user.rut.includes(this.rutFilterText.toLowerCase()))
+			);
 		},
 	},
 	mounted() {
@@ -319,7 +373,7 @@ export default {
 	},
 	methods: {
 		async initFetch() {
-     		await this.getFormattedSessions();
+			await this.getFormattedSessions();
 			const { amounts } = await this.$axios.$get('/dashboard/pay-mount');
 			this.psychologist = amounts;
 			const { transactions } = await this.$axios.$get('/transaction/get/all');
@@ -369,33 +423,32 @@ export default {
 			}
 		},
 		showUserSessions(item) {
-			this.userSessions = item.sessions
+			this.userSessions = item.sessions;
 			this.userSessions.forEach(session => {
-				session.name = item.name
-				session.email = item.email
-			})
-			this.dialog = true
+				session.name = item.name;
+				session.email = item.email;
+			});
+			this.dialog = true;
 		},
 		...mapMutations({
 			snackBar: 'Snackbar/showMessage',
 		}),
-    async getFormattedSessions() {
-      try {
-        const { data } = await this.$axios('/sessions/get-all-sessions-formatted', {
-          method: 'GET'
-        });
-        const { formattedSessions } = data;
-        this.sessions = formattedSessions;
-        return formattedSessions;
-      } catch (e) {
-        this.snackBar({
-          content: e,
-          color: 'error',
-        });
-      }
-    },
+		async getFormattedSessions() {
+			try {
+				const { data } = await this.$axios('/sessions/get-all-sessions-formatted', {
+					method: 'GET',
+				});
+				const { formattedSessions } = data;
+				this.sessions = formattedSessions;
+				return formattedSessions;
+			} catch (e) {
+				this.snackBar({
+					content: e,
+					color: 'error',
+				});
+			}
+		},
 	},
 };
 </script>
-<style>
-</style>
+<style></style>
