7fda31b3d6ba7edcebab156a98b02ac1459bcd69
Sebastián <sebastian.gonzalez.cass@usach.cl>
2023-03-05 20:20:58 -0300

    fix: api_url

--- api/server/utils/responses/functions.js
+++ api/server/utils/responses/functions.js
@@ -4,7 +4,7 @@ export const restResponse = (data, code, res) =>
 		...data,
 		status: true,
 		apiVersion: 'v1',
-		baseUrl: process.env.BASE_URL,
+		baseUrl: process.env.API_URL,
 	});
 
 export const conflictResponse = message => ({ code: 409, data: { message } });
